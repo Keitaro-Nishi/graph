1098e05e5487b606e988fa8a1f4d97d9f7edf3eb
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-18 15:53:20 +0900

    Initial Commit

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HelloWorldController extends Controller
+{
+    public function getIndex() {
+        echo 'Hello World';
+    }
+
+    public function getLogin() {
+    	echo 'Hello World';
+    }
+}
