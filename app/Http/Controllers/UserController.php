bdb812f9ad4501815b115e6e6aaa28ccf2561040
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-01-30 14:29:32 +0900

    a

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\DB;
+//use App\User;
+
+class UsersController extends Controller
+{
+	public function index(Request $request)
+	{
+		$users =DB::select('select * from users');
+		return view('users',['users'=>$users]);
+	}
+
+}
