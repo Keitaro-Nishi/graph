769c7d895a7567052a425ef168a9e7011515c79a
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-01-30 14:32:48 +0900

    k

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get ( '/', function () {
 	Auth::routes();
 
 	Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/users','UsersController@index');
+        Route::get('/ajax/{deletecode}','UserdeleteController@index');
