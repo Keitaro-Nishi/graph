94afba1710081c11bb4db339ed754c3dea9c1c1d
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-01-30 14:53:59 +0900

    a

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\support\Facades\DB;
 //use App\User;
 
-class UsersController extends Controller
+class UserController extends Controller
 {
 	public function index(Request $request)
 	{
