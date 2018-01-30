36a4545ff66d4417708ac4f0bb992729fa4caf6d
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-01-30 14:44:18 +0900

    a

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,1 +1,97 @@
 
+<!DOCTYPE html>
+<html>
+<meta name="description" content="ユーザー情報">
+<title>ユーザー情報</title>
+<link href="css/common.css" rel="stylesheet" />
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/jquery.bootgrid.css" rel="stylesheet" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.bootgrid.js"></script>
+
+<table id="grid-basic"
+	class="table table-condensed table-hover table-striped">
+	<thead>
+		<tr>
+			<th data-column-id="no" data-type="numeric" data-identifier="true"
+				data-width="3%" data-order="desc">id</th>
+			<th data-column-id="custid" data-width="7%">名前</th>
+			<th data-column-id="custname" data-width="10%">ユーザーID</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($users as $user)
+		<tr>
+			<td>{{$user->id}}</td>
+			<td>{{$user->name}}</td>
+			<td>{{$user->userid}}</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+
+<div class="container" align="center">
+	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
+</div>
+
+<script>
+
+			var rowIds = [];
+
+
+			$(function() {
+				$("#grid-basic").bootgrid({
+					selection : true,
+					multiSelect : true,
+					rowSelect : true,
+					keepSelection : true
+				}).on("selected.rs.jquery.bootgrid", function(e, rows) {
+					for (var i = 0; i < rows.length; i++) {
+						rowIds.push(rows[i].no);
+					}
+				}).on("deselected.rs.jquery.bootgrid", function(e, rows) {
+					for (var i = 0; i < rows.length; i++) {
+						rowIds.some(function(v, ii) {
+							if (v == rows[i].no)
+								rowIds.splice(ii, 1);
+						});
+					}
+				});
+			});
+
+
+			function drow() {
+				if(rowIds.length == 0){
+					alert("削除する行を選択してください");
+					return;
+				}
+				var successFlg = true;
+				var myRet = confirm("選択行を削除しますか？");
+				if ( myRet == true ){
+					for (var i = 0; i < rowIds.length; i++){
+						$.ajax({
+							type: "GET",
+							url: 'ajax/'+ rowIds[i],
+						}).then(
+							function(){
+							},
+							function(){
+								successFlg = false;
+							}
+						);
+					}
+					if( successFlg == true){
+						alert("削除しました");
+						location.reload();
+					}else{
+						alert("削除できませんでした");
+					}
+				}
+			}
+
+
+
+</script>
+</html>
+
