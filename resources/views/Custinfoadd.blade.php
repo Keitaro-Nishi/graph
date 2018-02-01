350d50a47f212e451c74ef191ac918ae0db36f26
sato taro <dsjvvknu@gmail.com>
2018-02-01 11:37:55 +0900

    a

--- resources/views/Custinfoadd.blade.php
+++ resources/views/Custinfoadd.blade.php
@@ -50,7 +50,7 @@
                                 <input id="password" type="password" class="form-control" name="password" required>
                             </div>
                         </div>
-						<div></div>
+						<br><div></div>
                         <div>
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
