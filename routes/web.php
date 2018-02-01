5ceadf810aebf8beed5535231e63979c2731e1d1
sato taro <dsjvvknu@gmail.com>
2018-02-01 09:37:01 +0900

    l

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get ( '/', function () {
 	Route::get('/home', 'HomeController@index')->name('home');
 	Route::get('/Custinfo','CustinfoController@index');
 	Route::get('/ajax/{deletecode}','CustinfoController@delete');
-
+	//Route::get(‘/Custinfoadd’,‘CustinfoaddController@add’);
+	//Route::post(‘/Custinfoadd’,‘CustinfoinsertController@insert’);
