fadbeb2ecd5860a106ef025446980bbda73e895c
sato taro <dsjvvknu@gmail.com>
2018-02-01 13:17:04 +0900

    a

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>Menu</title>
+<link href="css/common.css" rel="stylesheet" />
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/jquery.bootgrid.css" rel="stylesheet" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
+</head>
+<body>
+<div id="header"></div>
+<div class="container">
+	<div class="center-block">
+		<input type="button" class="btn btn-default" onclick="location.href='botlog.php'" value="ログ参照" />
+		<input type="button" class="btn btn-default" onclick="location.href='imagelog.php'" value="画像ログ参照" />
+		<input type="button" class="btn btn-default" onclick="location.href='shisetsu.php'" value="施設情報" />
+		<input type="button" class="btn btn-default" onclick="location.href='genre.php'" value="施設ジャンル" />
+		<input type="button" class="btn btn-default" onclick="location.href='opinion.php'" value="市政へのご意見" />
+		<input type="button" class="btn btn-default" onclick="location.href='test.php'" value="ボットテスト" />
+		<input type="button" class="btn btn-default" onclick="location.href='Custinfo'" value="ユーザー情報" />
+	</div>
+</div>
+<script>
+$(function() {
+	$("#header").load("header.html");
+
+});
+</script>
+</body>
+</html>
 
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
  * |
  */
 Route::get ( '/', function () {
-	return view ( 'welcome' );
+	return view ( 'menu' );
 } );
 	Route::get ( '/hello_world/index', 'HelloWorldController@getIndex' );
 	Route::get ( '/hello_world/login', 'HelloWorldController@getLogin' );
