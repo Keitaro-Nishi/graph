a405ceda98fc246223c11622ffc2aa1f06747676
sato taro <dsjvvknu@gmail.com>
2018-02-01 10:57:58 +0900

    k

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,9 @@
 						<!-- Authentication Links -->
 						@guest
 						<li><a href="{{ route('login') }}">Login</a></li>
-						<li><a href="{{ route('register') }}">Register</a></li>
+						<li><a href="{{ route('Custinfoadd') }}">Register</a></li>
 						@else
-						<li><a href="{{ route('register') }}">Register</a></li>
+						<li><a href="{{ route('Custinfoadd') }}">Register</a></li>
 						<li class="dropdown"><a href="#" class="dropdown-toggle"
 							data-toggle="dropdown" role="button" aria-expanded="false"
 							aria-haspopup="true"> {{ Auth::user()->name }} <span
