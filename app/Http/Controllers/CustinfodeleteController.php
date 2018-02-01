92289b471e719744e141fe0cc7a32ff0364deb9e
sato taro <dsjvvknu@gmail.com>
2018-02-01 10:42:26 +0900

    a

--- app/Http/Controllers/CustinfodeleteController.php
+++ app/Http/Controllers/CustinfodeleteController.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class CustinfodeleteController extends Controller
+{
+	public function delete(Request $request)
+	{
+		$deleteno = $request->deletecode;
+		DB::delete('delete from custinfo WHERE no=?',[$deleteno]);
+		return redirect('/Custinfo');
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get ( '/', function () {
 
 	Route::get('/home', 'HomeController@index')->name('home');
 	Route::get('/Custinfo','CustinfoController@index');
-	Route::get('/ajax/{deletecode}','CustinfoController@delete');
+	Route::get('/ajax/{deletecode}','CustinfodeleteController@delete');
 	Route::get('/Custinfoadd','CustinfoaddController@add');
 	Route::post('/Custinfoadd','CustinfoaddController@insert');
 
