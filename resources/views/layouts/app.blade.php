0f78e8dabdb34d49bc8f2bb19a9227378d963e88
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-05 17:50:36 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,8 +47,8 @@
 						<li><a href="{{ route('register') }}">Register</a></li>
 						<li class="dropdown"><a href="#" class="dropdown-toggle"
 							data-toggle="dropdown" role="button" aria-expanded="false"
-							aria-haspopup="true"> {{ Auth::user()->name }} <span
-								class="caret"></span>
+							aria-haspopup="true"> {{ Auth::user()->name }}
+							<span class="caret"></span>
 						</a>
 
 							<ul class="dropdown-menu">
@@ -57,13 +57,14 @@
 
 								<li><a href="{{ route('logout') }}"
 									onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-										Logout </a>
+                                                     document.getElementById('logout-form').submit();"> Logout </a>
 
 									<form id="logout-form" action="{{ route('logout') }}"
 										method="POST" style="display: none;">{{ csrf_field() }}</form>
 								</li>
-							</ul></li> @endguest
+							</ul>
+						</li>
+						 @endguest
 					</ul>
 				</div>
 			</div>
