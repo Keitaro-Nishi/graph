d1c292d49ce77c5b8040dcb78a0ffcaee0cbe824
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-02-05 18:17:21 +0900

    a

--- app/Http/Controllers/UseraddController.php
+++ app/Http/Controllers/UseraddController.php
@@ -1,1 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CustomeraddController extends Controller {
+
+
+	public function add(Request $request) {
+		return view ('useradd');
+	}
+
+
+	public function insert(Request $request) {
+
+	     $userid= $request->userid;
+       $name= $request->name;
+       $organization= $request->organization;
+       $password = bcrypt($request->password);
+
+
+       DB::insert('insert into users (citycode,userid,name,email,organization,password,role,reserve) values (?,?,?,?,?,?,?,?)', ['',$userid,$name,'',$organization,$password,1,'']);
+       return redirect('/users');
+	}
+}
 
