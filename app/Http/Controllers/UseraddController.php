3e0f3627acb97dc9403e757873c255b2eb64b41c
sato taro <dsjvvknu@gmail.com>
2018-02-06 09:29:34 +0900

    k

--- app/Http/Controllers/UseraddController.php
+++ app/Http/Controllers/UseraddController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class CustomeraddController extends Controller {
+class UseraddController extends Controller {
 
 
 	public function add(Request $request) {
