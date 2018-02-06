e95beefcaee3acc06ba9a72a40119695fd4f8038
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-06 15:50:53 +0900

    s

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,11 @@
  * | contains the "web" middleware group. Now create something great!
  * |
  */
-/*
+
 Route::get ( '/', function () {
 	return view ( 'welcome' );
 } );
-*/
+
 Route::get ( '/Menu', function () {
 	return view ( 'Menu' );
 } );
