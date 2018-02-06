b02931b3102710376c3eb45a6ed671d7fdc5e848
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-06 14:35:27 +0900

    s

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,42 +5,51 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+class LoginController extends Controller {
+	/*
+	 * |--------------------------------------------------------------------------
+	 * | Login Controller
+	 * |--------------------------------------------------------------------------
+	 * |
+	 * | This controller handles authenticating users for the application and
+	 * | redirecting them to your home screen. The controller uses a trait
+	 * | to conveniently provide its functionality to your applications.
+	 * |
+	 */
 
-    use AuthenticatesUsers;
+	use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+	/**
+	 * Where to redirect users after login.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
 
-        return redirect('/login');
-    }
+	/*
+	 * public function __construct()
+	 * {
+	 * $this->middleware('guest')->except('logout');
+	 * }
+	 */
 
-    public function username()
-    {
-    	return 'userid';
-    }
+	public function logout(Request $request) {
+		$this->guard ()->logout ();
+
+		$request->session ()->flush ();
+
+		$request->session ()->regenerate ();
+
+		return redirect ( '/redirect/path/to/after/regist/' );
+	}
+
+	public function username() {
+		return 'userid';
+	}
 }
