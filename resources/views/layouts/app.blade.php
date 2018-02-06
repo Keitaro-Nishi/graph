e747acc75869f14606a06ff23bbe3d03ace0fdd0
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-06 13:51:06 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 					</button>
 
 					<!-- Branding Image -->
-					<a class="navbar-brand" href="{{ url('/') }}"> {{
+					<a class="navbar-brand" href="{{ url('/login') }}"> {{
 						config('app.name', 'Laravel') }} </a>
 				</div>
 
