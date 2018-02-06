a8ac745961de17ae37c7a5e4c1c1a2577de012fc
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-06 14:48:35 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,6 @@
 					<ul class="nav navbar-nav navbar-right">
 						<!-- Authentication Links -->
 						@guest
-						<li><a href="{{ route('login') }}">Login</a></li>
 						<li><a href="{{ route('register') }}">Register</a></li>
 						@else
 						<li><a href="{{ route('register') }}">Register</a></li>
