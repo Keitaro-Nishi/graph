cac3471e2663c26061f70369562225fea47c25f5
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-06 12:59:38 +0900

    s

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -46,6 +46,8 @@ class RegisterController extends Controller {
 	protected function validator(array $data) {
 		return Validator::make ( $data, [
 				'name' => 'required|string|max:255',
+				'userid' => 'required|string|max:255|unique:users',
+				'organization' => 'required|string|max:255',
 				'password' => 'required|string|min:6|confirmed'
 		] );
 	}
