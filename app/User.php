695ad025290c2d6b485d262326783acb1e1bfa6a
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-02-06 13:35:36 +0900

    s

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-    		'name','userid','password','organization', 'role', 'reserve'
+    		'name', 'userid', 'password', 'organization', 'role', 'reserve'
     ];
 
     /**
