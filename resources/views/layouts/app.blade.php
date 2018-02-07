797cffdb12680bf10579cfb422b6ce93e5c6eb6e
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-07 13:53:47 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
 						<li><a href="{{ route('register') }}">Register</a></li>
 						<li class="dropdown"><a href="#" class="dropdown-toggle"
 							data-toggle="dropdown" role="button" aria-expanded="false"
-							aria-haspopup="true"> {{ Auth::user()->name }}
+							aria-haspopup="true"> {{ Auth::user()->role }}
 							<span class="caret"></span>
 						</a>
 
