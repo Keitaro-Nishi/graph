e4d6db9c6234ed5531caa35b177ef645c20cad38
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-07 16:49:27 +0900

    s

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,8 @@ class RegisterController extends Controller {
 				'userid' => $data ['userid'],
 				'organization' => $data ['organization'],
 				'password' => bcrypt ( $data ['password'] ),
-				'role' => '1',
+				'role' => $data ['role']
+				//'role' => Auth::user()->role + 1???????
 		] );
 	}
 }
