a4a9da4e82d7d682379f24cb15ea920c48d97d2d
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-07 13:02:38 +0900

    s

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-md-push-3 col-md-6">
+
 			<br>
 			<div class="form-wrap">
 				<div class="text-center">
 					<h1>Login</h1>
 			<br>
+			<div class="col-xs-12 col-md-push-3 col-md-6">
 				<div class="panel panel-default">
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
