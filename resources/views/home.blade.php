2e7378c4fe3a239ff1db5d7dc414032ed247e567
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-07 15:10:26 +0900

    s

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 				<div class="panel-heading">Dashboard</div>
 
 				<div class="panel-body">
-					@if (Auth::user()->role == "0" )
+					@if (Auth::user()->role == "1" )
 					<div class="alert alert-success">
 						<p>You are logged in!</p>
 					</div>
