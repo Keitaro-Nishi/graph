c4f7993943d04f83a3e78165ab3877fc8b7b51e3
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-07 11:49:38 +0900

    s

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-2">
 
 			<div class="form-wrap">
 				<div class="text-center">
