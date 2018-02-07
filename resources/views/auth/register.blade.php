8796b49ee81d835a7fbddfd903c94e213b903736
sato taro <dsjvvknu@gmail.com>
2018-02-07 15:25:15 +0900

    k

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         {{ csrf_field() }}
 
 
-						@if (Auth::user()->role == 0)
+						@if (Auth::user()->role == 1)
 						<div style="display: none">
 						@endif
 
@@ -30,7 +30,7 @@
                             </div>
                         </div>
 
-                        @if (Auth::user()->role == 0)
+                        @if (Auth::user()->role == 1)
                         </div>
                         @endif
 
