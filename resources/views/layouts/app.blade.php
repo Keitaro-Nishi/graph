5d4a82db1a1fe70ae1b8413f80047ab3d1a391a6
sato taro <dsjvvknu@gmail.com>
2018-02-07 13:28:51 +0900

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
 						<li><a href="{{ route('register') }}">Register</a></li>
 						<li class="dropdown"><a href="#" class="dropdown-toggle"
 							data-toggle="dropdown" role="button" aria-expanded="false"
-							aria-haspopup="true"> {{ Auth::user()->citycode }}
+							aria-haspopup="true"> {{ Auth::user()->name}}
 							<span class="caret"></span>
 						</a>
 
