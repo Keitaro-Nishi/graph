f9ff97d63be9acaca2001bd88ad4c57a435ffaa2
sato taro <dsjvvknu@gmail.com>
2018-02-08 10:10:26 +0900

    k

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller {
 	/*
@@ -68,7 +69,8 @@ class RegisterController extends Controller {
 	protected function create(array $data) {
 		return User::create ( [
 				//'citycode' =>$data ['citycode'],
-				'citycode' => '55555',
+				//'citycode' => '55555',
+				'citycode' => DB::select('select role from users WHERE role=?',[0]),
 				'name' => $data ['name'],
 				'userid' => $data ['userid'],
 				'organization' => $data ['organization'],
