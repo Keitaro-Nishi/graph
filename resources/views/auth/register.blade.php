8414f4ed3ff1ae071cbfaf164d07f3b1f6faf867
sato taro <dsjvvknu@gmail.com>
2018-02-09 11:38:41 +0900

    a

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -83,22 +83,8 @@
 							</div>
 						</div>
 
-						<div class="form-group{{ $errors->has('organization') ? ' has-error' : '' }}">
-							<label for="organization" class="col-md-4 control-label">ユーザーID</label>
-
-							<div class="col-md-6">
-								<input id="organization" type="text" class="form-control" name="organization" value="{{ old('organization') }}" required>
-
-								@if ($errors->has('organization'))
-									<span class="help-block">
-										<strong>{{ $errors->first('organization') }}</strong>
-									</span>
-								@endif
-							</div>
-						</div>
-
 						<div class="form-group{{ $errors->has('userid') ? ' has-error' : '' }}">
-							<label for="userid" class="col-md-4 control-label">組織名</label>
+							<label for="userid" class="col-md-4 control-label">ユーザーID</label>
 
 							<div class="col-md-6">
 								<input id="userid" type="text" class="form-control" name="userid" value="{{ old('userid') }}" required>
@@ -112,6 +98,19 @@
 						</div>
 
 
+						<div class="form-group{{ $errors->has('organization') ? ' has-error' : '' }}">
+							<label for="organization" class="col-md-4 control-label">組織名</label>
+
+							<div class="col-md-6">
+								<input id="organization" type="text" class="form-control" name="organization" value="{{ old('organization') }}" required>
+
+								@if ($errors->has('organization'))
+									<span class="help-block">
+										<strong>{{ $errors->first('organization') }}</strong>
+									</span>
+								@endif
+							</div>
+						</div>
 
 						<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 							<label for="password" class="col-md-4 control-label">パスワード</label>
