0e5e637b7a6d15315f00ff04f2f2d6279adfd2f1
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-09 16:02:29 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,6 @@
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/jquery.bootgrid.js"></script>
-
 </head>
 <body>
 	<div id="app">
