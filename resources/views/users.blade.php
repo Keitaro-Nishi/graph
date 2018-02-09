4a88e7524b6c2f31a0312548b76d618772b42104
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-09 14:36:27 +0900

    s

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
+@section('content')
 <!DOCTYPE html>
-<html>
 <head>
 <meta name="description" content="ユーザー情報">
 <title>ユーザー情報</title>
@@ -13,7 +13,6 @@
 <script src="js/jquery.bootgrid.js"></script>
 </head>
 <body>
-
 <table id="grid-basic"
 	class="table table-condensed table-hover table-striped">
 	<thead>
@@ -95,4 +94,4 @@
 			}
 </script>
 </body>
-</html>
+@section
