d7ebe32136ab448a86f3e80653df3ea76798621b
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-13 09:52:25 +0900

    s

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,6 +1,17 @@
-@extends('layouts.app')
-
-@section('content')
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="description" content="ユーザー情報">
+<title>ユーザー情報</title>
+<link href="css/common.css" rel="stylesheet" />
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/jquery.bootgrid.css" rel="stylesheet" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.bootgrid.js"></script>
+</head>
+<body>
+<div id="header"></div>
 <table id="grid-basic"
 	class="table table-condensed table-hover table-striped">
 	<thead>
@@ -29,7 +40,6 @@
 
 <script>
 			var rowIds = [];
-
 
 			$(function() {
 				$("#header").load("header.html");
@@ -81,4 +91,6 @@
 				}
 			}
 </script>
-@endsection
+</body>
+</html>
+
