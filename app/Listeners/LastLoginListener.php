ce5bb7b3dec6d2734d3679caf76d64aad0d8a959
sato taro <dsjvvknu@gmail.com>
2018-02-13 16:27:50 +0900

    k

--- app/Listeners/LastLoginListener.php
+++ app/Listeners/LastLoginListener.php
@@ -35,7 +35,7 @@ class LastLoginListener
     {
 
       $user = Auth::user();
-      $user->last_login_at = Carbon::now()->format('Y-m-d');
+      $user->last_login_at = Carbon::now()->format('Y-m-d-h-i-s');
       $user->save();
 
 		/*
