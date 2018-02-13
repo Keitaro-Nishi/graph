8416b45e47bde4fee754202baf9bd0a6677c4a4c
sato taro <dsjvvknu@gmail.com>
2018-02-13 11:05:26 +0900

    k

--- app/Listeners/LastLogoutListener.php
+++ app/Listeners/LastLogoutListener.php
@@ -2,7 +2,7 @@
 
 namespace App\Listeners;
 
-use App\Events\Logined;
+use App\Events\Logouted;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Auth;
