dc2d3f30e6cfe9051bad4c7223c44370b60cd4ad
sato taro <dsjvvknu@gmail.com>
2018-02-14 10:19:22 +0900

    k

--- app/Http/Controllers/LogindataController.php
+++ app/Http/Controllers/LogindataController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
-
 class LogindataController
 {
 	public function index(Request $request)
@@ -14,7 +13,6 @@ class LogindataController
 		$logindata =DB::select('select * from logindata');
 		return view('logindata',['logindata'=>$logindata]);
 	}
-
 
 }
 
