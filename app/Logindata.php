574e510b96e497ac94efddef0b00a1412b169398
sato taro <dsjvvknu@gmail.com>
2018-02-14 13:32:36 +0900

    a

--- app/Logindata.php
+++ app/Logindata.php
@@ -3,19 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-//use Illuminate\Notifications\Notifiable;
+
 
 class Logindata extends Model
 {
-	/*
-	use Notifiable;
+
+
 
 	protected $fillable = [
 
-			'id', 'userid','last_login_at'
+			'id', 'userid','last_login_at','last_logout_at'
 
 	];
-	*/
+
 
 	protected $table = 'logindata';
 
