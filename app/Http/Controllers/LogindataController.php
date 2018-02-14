74eec230973f8f93c26795bae73a84bcc0704d57
sato taro <dsjvvknu@gmail.com>
2018-02-14 13:47:52 +0900

    k

--- app/Http/Controllers/LogindataController.php
+++ app/Http/Controllers/LogindataController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Logindata;
 
 
 class LogindataController
 {
 	public function index(Request $request)
 	{
+		/*
 		$logindata =DB::select('select * from logindata');
+		return view('logindata',['logindata'=>$logindata]);
+		*/
+
+		$logindata = App\Logindata::all();
 		return view('logindata',['logindata'=>$logindata]);
 	}
 
