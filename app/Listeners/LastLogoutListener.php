4005335c155f487e62930fefc229aede9063f252
sato taro <dsjvvknu@gmail.com>
2018-02-14 10:44:15 +0900

    a

--- app/Listeners/LastLogoutListener.php
+++ app/Listeners/LastLogoutListener.php
@@ -7,6 +7,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 /**
  * ログアウト完了時に発火するイベントをキャッチするリスナークラス
