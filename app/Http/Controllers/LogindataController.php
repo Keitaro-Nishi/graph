7e3037295b5f89381d740359eb91da25d319e765
sato taro <dsjvvknu@gmail.com>
2018-02-14 13:51:17 +0900

    a

--- app/Http/Controllers/LogindataController.php
+++ app/Http/Controllers/LogindataController.php
@@ -11,13 +11,15 @@ class LogindataController
 {
 	public function index(Request $request)
 	{
-		/*
+
 		$logindata =DB::select('select * from logindata');
 		return view('logindata',['logindata'=>$logindata]);
-		*/
 
+
+		/*
 		$logindata = App\Logindata::all();
 		return view('logindata',['logindata'=>$logindata]);
+		*/
 	}
 
 }
