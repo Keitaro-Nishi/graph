38c2222f09d7376ce40ff0813e34aa7e9a86e7fe
sato taro <dsjvvknu@gmail.com>
2018-02-14 13:20:45 +0900

    k

--- app/Listeners/LastLoginListener.php
+++ app/Listeners/LastLoginListener.php
@@ -9,15 +9,14 @@ use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Logindata;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+
 
 /**
  * ログイン完了時に発火するイベントをキャッチするリスナークラス
  * Class LastLoginListener
  * @package App\Listeners
  */
-class LastLoginListener extends Controller
+class LastLoginListener
 {
     /**
      * Create the event listener.
@@ -38,20 +37,21 @@ class LastLoginListener extends Controller
     public function handle(Logined $event)
     {
 
-    /*
+
     $user = Auth::user();
     $userid = $user->userid;
     $last_login_at = Carbon::now();
 
     DB::insert('insert into logindata (userid,last_login_at) values (?,?)', [$userid,$last_login_at]);
-	*/
 
+		/*
 	    $user = Auth::user();
 	    $logindata = new Logindata;
 	    $logindata->userid = $user->userid;
 	    $logindata->last_login_at = Carbon::now();
 
 	    $logindata->save();
+	    */
 
 
     }
