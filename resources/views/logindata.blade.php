9d8fa6d5fdd93ad424b0cd029eaaf6018bbb9a98
sato taro <dsjvvknu@gmail.com>
2018-02-14 16:26:45 +0900

    a

--- resources/views/logindata.blade.php
+++ resources/views/logindata.blade.php
@@ -28,7 +28,7 @@
 		<tr>
 			<td>{{$infomation->id}}</td>
 			<td>{{$infomation->userid}}</td>
-			<td><li><span class="glyphicon glyphicon-list"></span></li></td>
+			<td><span class="glyphicon glyphicon-list"></span></td>
 			 <!--<td><span  class="glyphicon glyphicon-log-in"></span></td>  ログ参照 {{$infomation->classification}}-->
 			<td>{{$infomation->time}}</td>
 		</tr>
