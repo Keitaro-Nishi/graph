51f10a5950a7c8e29786e01dedbe8bd91a62672d
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-14 16:47:18 +0900

    s

--- resources/views/common/_script.blade.php
+++ resources/views/common/_script.blade.php
@@ -1,7 +1,4 @@
 <!--
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/jquery.bootgrid.js"></script>
 <script type="text/javascript">
 $(function(){
 
