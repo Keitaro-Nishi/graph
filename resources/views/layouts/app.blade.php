4664cbe90ce4ccdc07677ff23d8371cb4a424681
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-15 10:22:29 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,9 +57,7 @@
 
 							<ul class="dropdown-menu">
 								<li><a href="{{ route('users') }}">Users</a></li>
-								<li>
-								<span class="glyphicon glyphicon-log-out">
-								<a href="{{ route('logout') }}"
+								<li><a href="{{ route('logout') }}"
 									onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();"> Logout </a>
 
