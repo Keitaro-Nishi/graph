bd7b3248c07a5b1065e0b267ac827e0e2158dccf
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-15 10:15:15 +0900

    s

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Auth::routes ();
 Route::get ( '/register', 'Auth\RegisterController@showRegistrationForm')->name('register')->middleware('role');
 
 Route::get ( '/home', 'HomeController@index' )->name ( 'home' );
-Route::get ( '/users', 'UserController@index' )->name ( 'users' )->middleware('auth');;
+Route::get ( '/users', 'UserController@index' )->name ( 'users' )->middleware('auth');
 Route::get ( '/ajax/{deletecode}', 'UserController@delete' );
 	//Route::get('/ajax/{deletecode}','UserdeleteController@delete');
 	//Route::get('/useradd','UseraddController@add');
