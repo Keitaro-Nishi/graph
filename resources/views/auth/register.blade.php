dca96e2d5c8f577e74108adde6f34f463ce86442
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-15 10:44:15 +0900

    s

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -116,7 +116,7 @@
 							</div>
 						</div>
 
-						<div class="form-group" align="center">
+						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-primary">
 									登録
