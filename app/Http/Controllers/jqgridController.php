9dee13c7f2f52334d2732f1473f811d448ebe455
sato taro <dsjvvknu@gmail.com>
2018-02-15 17:18:47 +0900

    a

--- app/Http/Controllers/jqgridController.php
+++ app/Http/Controllers/jqgridController.php
@@ -10,12 +10,14 @@ class jqgridController extends Controller
     public function index()
     {
 
-    	$tests =DB::select('select * from test');
-    	/*$id =DB::select('select id from test');
+    	//$tests =DB::select('select * from test');
+    	$id =DB::select('select id from test');
     	$name =DB::select('select name from test');
-    	$price =DB::select('select price from test');*/
+    	$price =DB::select('select price from test');
     	header("Content-Type: application/json charset=utf-8");
-    	echo json_encode( $tests );
+    	echo json_encode( $id );
+    	echo json_encode( $name );
+    	echo json_encode( $price );
 
         return view('jqgrid');
     }
