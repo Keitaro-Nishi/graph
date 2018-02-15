8229388d9ded0ba2cfa60480940cc9d15b19ccd3
sato taro <dsjvvknu@gmail.com>
2018-02-15 16:47:58 +0900

    a

--- resources/views/jqgrid.blade.php
+++ resources/views/jqgrid.blade.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <title>jqgridサンプル</title>
-        <link type="text/css" media="screen" href="css/jquery-ui.css" rel="stylesheet" />
-    	<link type="text/css" media="screen" href="css/ui.jqgrid.css" rel="stylesheet" />
-    	<script type="text/javascript" src="js/jquery-1.11.0.min.js" ></script>
-    	<script type="text/javascript" src="js/jquery-ui.min.js" ></script>
-    	<script type="text/javascript" src="js/jquery.jqGrid.min.js" ></script>
-    	<script type="text/javascript" src="js/grid.locale-ja.js" ></script>
-        <script type="text/javascript">
-            $(function(){
-                $("#tbl1").jqGrid({
-                     url: "jqgridController.php"
-                    ,datatype: "json"
-                    ,type: "POST"
-                    ,colNames:["id", "名前", "価格"]
-                    ,colModel:[ {name:"id"}
-                               ,{name:"name"}
-                               ,{name:"price"}
-                     ]
-                    ,width: "auto"
-                    ,height: "auto"
+<meta charset="UTF-8">
+<title>Insert title here</title>
+    <link type="text/css" media="screen" href="css/jquery-ui.css" rel="stylesheet" />
+    <link type="text/css" media="screen" href="css/ui.jqgrid.css" rel="stylesheet" />
+    <script type="text/javascript" src="js/jquery-1.11.0.min.js" ></script>
+    <script type="text/javascript" src="js/jquery-ui.min.js" ></script>
+    <script type="text/javascript" src="js/jquery.jqGrid.min.js" ></script>
+    <script type="text/javascript" src="js/grid.locale-ja.js" ></script>
 
-                    }
-                });
-            });
+    <script type="text/javascript">
+    jQuery(document).ready(function()
+    {
+
+    	jQuery("#list").jqGrid
+    	({
+                     url: "jqgridController.php",
+                     datatype: "json",
+                     type: "POST",
+                     colNames:['ID', '名前', '価格'],
+                     colModel:[ {name:"id"},
+                                {name:"name"},
+                                {name:"price"}
+                     ]
+				    multiselect: true,
+				    caption: 'テスト'
+
+        });
+     });
         </script>
     </head>
 <body>
-  <table id="tbl1">
+  <table id="list">
   </table>
 </body>
 </html>
