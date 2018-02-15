a16b7c1660d97e449b0e625bb0a612a057ce286f
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-15 10:11:10 +0900

    s

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 //use App\User;
 
 class UserController
 {
-
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
 
 	public function index(Request $request)
 	{
