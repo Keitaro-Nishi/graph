ec24bb0335f049aff3e1204807d8be3baa04b5a7
sato taro <dsjvvknu@gmail.com>
2018-02-16 10:43:47 +0900

    a

--- resources/views/jqgrid.blade.php
+++ resources/views/jqgrid.blade.php
@@ -2,13 +2,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>テスト</title>
     <link type="text/css" media="screen" href="css/jquery-ui.css" rel="stylesheet" />
     <link type="text/css" media="screen" href="css/ui.jqgrid.css" rel="stylesheet" />
     <script type="text/javascript" src="js/jquery-1.11.0.min.js" ></script>
     <script type="text/javascript" src="js/jquery-ui.min.js" ></script>
-    <script type="text/javascript" src="js/jquery.jqGrid.min.js" ></script>
     <script type="text/javascript" src="js/grid.locale-ja.js" ></script>
+    <script type="text/javascript" src="js/jquery.jqGrid.min.js" ></script>
+
 
     <script type="text/javascript">
     jQuery(document).ready(function()
@@ -27,6 +28,8 @@
                      ]
 				    multiselect: true,
 				    caption: 'テスト',
+				    width: "auto",
+	                height: "auto",
 				    'loadError' : function (xhr, status, error){
 				    alert(error);
 				    }
