87cd8ab0b39c2c0be57def529e6eedb5eef90264
sato taro <dsjvvknu@gmail.com>
2018-02-16 17:48:37 +0900

    k

--- resources/views/logindata.blade.php
+++ resources/views/logindata.blade.php
@@ -42,7 +42,7 @@
 
 			var tr = $("table tr");//全行を取得
 			var rowCount =tr.length;
-			alert(rowCount);
+
 
 			$(function() {
 				$("#header").load("header.html");
@@ -52,7 +52,7 @@
 					rowSelect: true,
 					formatters: {
 				      "icons": function($column, $row) {
-
+				    	       alert(rowCount);
 				    	  		return "<span class='glyphicon glyphicon-log-in'></span>";
 
 								    	 /*for (var i = 0; i < rows.length; i++){
