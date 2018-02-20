68a2b61296dbd725d00997264a642f965b9060d4
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-20 16:58:05 +0900

    s

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,8 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="form-wrap">
-			<div class="text-center">
-				<h1>Login</h1>
-			</div>
-		</div>
+	<br>
+	<br>
 	<br>
 		<div class="col-xs-12 col-md-push-3 col-md-6">
 			<div class="panel panel-default">
@@ -17,7 +14,7 @@
 						{{ csrf_field() }}
 						<br>
 						<div class="form-group{{ $errors-> has('userid') ? ' has-error' : '' }}">
-							<label for="userid" class="col-md-4 control-label">User ID</label>
+							<label for="userid" class="col-md-4 control-label">ユーザーID</label>
 
 							<div class="col-md-6">
 								<input id="userid" type="text" class="form-control" name="userid" value="{{ old('userid') }}" required autofocus>
@@ -32,7 +29,7 @@
 
 						<div
 							class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-							<label for="password" class="col-md-4 control-label">Password</label>
+							<label for="password" class="col-md-4 control-label">パスワード</label>
 
 							<div class="col-md-6">
 								<input id="password" type="password" class="form-control" name="password" required>
@@ -56,7 +53,7 @@
 
 						<div class="form-group">
 							<div class="col-md-8 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
+								<button type="submit" class="btn btn-primary">ログイン</button>
 
 								<a class="btn btn-link" href="{{ route('password.request') }}">
 									Forgot Your Password? </a>
