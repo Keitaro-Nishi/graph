dbe649d08e9efca41c624283e837dd508446dc09
sato taro <dsjvvknu@gmail.com>
2018-02-21 17:54:19 +0900

    l

--- resources/views/opinion.blade.php
+++ resources/views/opinion.blade.php
@@ -42,7 +42,7 @@
 	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog" value="モーダル表示" />
 </div>
 </div>
-<!--
+
 <div class="modal" id="shosaiDialog"  tabindex="-1">
 	<div class="modal-dialog">
 		<div class="modal-content" style="width:740px; margin-left: -20px;">
@@ -113,7 +113,7 @@
 		</div>
 	</div>
 </div>
--->
+
 <script>
 			var rowIds = [];
 			var dbvalue = [];
