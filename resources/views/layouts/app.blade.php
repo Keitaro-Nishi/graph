cd08050a4b23a14f92250890086af74cbfb647fd
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-21 13:41:30 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>{{ config('app.name', 'ViewLog') }}</title>
+<title>ViewLog</title>
 
 <!-- Styles -->
 <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
