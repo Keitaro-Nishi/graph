3be0ac338195dc96efa2705cca789237405f7eba
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-21 09:25:24 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
 						@endif
 						<li class="dropdown"><a href="#" class="dropdown-toggle"
 							data-toggle="dropdown" role="button" aria-expanded="false"
-							aria-haspopup="true"> {{ Auth::user()->name}}
+							aria-haspopup="true" class="glyphicon glyphicon-user">
 							<span class="caret"></span>
 						</a>
 
@@ -65,7 +65,7 @@
 								<li><a href="{{ route('users') }}">Users</a></li>
 								<li><a href="{{ route('logout') }}"
 									onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();" class="glyphicon glyphicon-user"></a>
+                                                     document.getElementById('logout-form').submit();"> Logout </a>
 
 									<form id="logout-form" action="{{ route('logout') }}"
 										method="POST" style="display: none;">{{ csrf_field() }}</form>
