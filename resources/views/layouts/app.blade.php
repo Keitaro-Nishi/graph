cf02fccf65450988c94e4788458bc3c5c6ad732c
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-22 10:48:28 +0900

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
 								<li><a href="{{ route('register') }}">画像ログ参照</a></li>
 								<li><a href="{{ route('register') }}">施設情報</a></li>
 								<li><a href="{{ route('register') }}">施設ジャンル</a></li>
-								<li><a href="{{ route('register') }}">ご意見ログ</a></li>
+								<li><a href="{{ route('opinion') }}">ご意見ログ</a></li>
 								<li><a href="{{ route('register') }}">属性情報</a></li>
 								<li><a href="{{ route('register') }}">メッセージ管理</a></li>
 								@if (Auth::user()->role == 0 or Auth::user()->role == 1)
@@ -83,7 +83,7 @@
 							<ul class="dropdown-menu">
 								@if (Auth::user()->role == 0 or Auth::user()->role == 1)
 								<li><a href="{{ route('users') }}">ユーザー管理</a></li>
-								<li><a href="{{ route('register') }}">ログイン情報</a></li>
+								<li><a href="{{ route('logindata') }}">ログイン情報</a></li>
 								<li><a href="{{ route('register') }}">Register</a></li>
 								@endif
 								<li><a href="{{ route('logout') }}"
