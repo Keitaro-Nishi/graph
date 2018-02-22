e152a1a2429ad9fbb28c2988b565906add0c1efa
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-22 10:22:20 +0900

    s

--- resources/views/botlog.blade.php
+++ resources/views/botlog.blade.php
@@ -88,8 +88,13 @@
 					}
 				}
 			}
+</script>
+		<div class="container" align="center">
+			<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
+			<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog" value="モーダル表示" />
+		</div>
 
-			<div class="modal" id="shosaiDialog"  tabindex="-1">
+		<div class="modal" id="shosaiDialog"  tabindex="-1">
 			<div class="modal-dialog">
 				<div class="modal-content" style="width:740px; margin-left: -20px;">
 					<div class="modal-header">
@@ -140,5 +145,111 @@
 				</div>
 			</div>
 		</div>
+
+<script>
+var rowIds = [];
+var dbvalue = [];
+var shosai_idx = 0;
+$(function() {
+	dbvalue = <?php echo json_encode($dbvalue); ?>;
+	var h = $(window).height();
+	$('#wrap').css('display','none');
+	$('#loader-bg ,#loader').height(h).css('display','block');
+	$("#header").load("header.html");
+	$("#grid-basic").bootgrid({
+		selection: true,
+		multiSelect: true,
+	    keepSelection: true,
+	    formatters: {
+	        "details": function($column, $row) {
+	        	return "<input type='button' class='btn btn-default' value='詳細' onclick='detailwin("  + $row.no + ")' > ";
+             }
+	    }
+	}).on("selected.rs.jquery.bootgrid", function(e, rows)
+	{
+	    for (var i = 0; i < rows.length; i++)
+	    {
+	        rowIds.push(rows[i].no);
+	    }
+	    //alert("Select: " + rowIds.join(","));
+	}).on("deselected.rs.jquery.bootgrid", function(e, rows)
+	{
+	    for (var i = 0; i < rows.length; i++)
+	    {
+	    	rowIds.some(function(v, ii){
+	    	    if (v==rows[i].no) rowIds.splice(ii,1);
+	    	});
+	        //rowIds.push(rows[i].no);
+	    }
+	    //alert("Deselect: " + rowIds.join(","));
+	});
+});
+$(window).load(function () { //全ての読み込みが完了したら実行
+	  $('#loader-bg').delay(900).fadeOut(800);
+	  $('#loader').delay(600).fadeOut(300);
+	  $('#wrap').css('display', 'block');
+});
+function drow() {
+	if(rowIds.length == 0){
+		alert("削除する行を選択してください");
+		return;
+	}
+	var myRet = confirm("選択行を削除しますか？");
+	if ( myRet == true ){
+		$.ajax({
+			type: "POST",
+			url: "botlogdel.php",
+			data:{
+				"no" : rowIds
+			}
+		}).done(function (response) {
+			result = JSON.parse(response);
+			if(result == "OK"){
+				alert("削除しました");
+				location.reload();
+			}else{
+				alert("削除できませんでした");
+			}
+	    }).fail(function () {
+	        alert("削除できませんでした");
+	    });
+	}
+}
+function detailwin(value){
+	document.getElementById("btn_modal").click();
+	for (var i = 0; i < dbvalue.length; i++){
+		if(dbvalue[i][0] == value){
+			shosai_idx = i;
+			modal_mod(i);
+		}
+	}
+}
+function shosai_back(){
+	shosai_idx = shosai_idx - 1;
+	modal_mod(shosai_idx);
+}
+function shosai_next(){
+	shosai_idx = shosai_idx + 1;
+	modal_mod(shosai_idx);
+}
+function modal_mod(index){
+	document.getElementById('dia_no').value  = dbvalue[index][0];
+	var idate = dbvalue[index][1].substr(0,4) + "/" + dbvalue[index][1].substr(4,2) + "/" + dbvalue[index][1].substr(6,2) + " " + dbvalue[index][1].substr(8,2) + ":" + dbvalue[index][1].substr(10,2);
+	document.getElementById('dia_date').value = idate;
+	document.getElementById('dia_user').value  = dbvalue[index][2];
+	document.getElementById('dia_que').innerHTML  = dbvalue[index][3];
+	document.getElementById('dia_ans').innerHTML  = dbvalue[index][4];
+	if(index == 0){
+		document.getElementById("sback").disabled = "true";
+	}else{
+		document.getElementById("sback").disabled = "";
+	}
+	if(index == dbvalue.length - 1){
+		document.getElementById("snext").disabled = "true";
+	}else{
+		document.getElementById("snext").disabled = "";
+	}
+}
+</script>
 </script>
 @endsection
