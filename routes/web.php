6150ca2ed5ae30b9f09c7de4ccb19339e63981fe
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-22 11:21:29 +0900

    s

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,14 @@ Route::get('/', function () { return redirect('/login'); });
 Auth::routes ();
 Route::get ( '/register', 'Auth\RegisterController@showRegistrationForm')->name('register')->middleware('role');
 
-Route::get ( '/home', 'HomeController@index' )->name ( 'home' );
+Route::get ( '/home', 'HomeController@index' )->name ( 'home' )->middleware('auth');
+
 Route::get ( '/users', 'UserController@index' )->name ( 'users' )->middleware('auth');
 Route::get ( '/users/{deletecode}', 'UserController@delete' );
+
 Route::get ( '/botlog', 'BotlogController@index' )->name ( 'botlog' )->middleware('auth');
-Route::get ( '/logindata', 'LogindataController@index' )->name ( 'logindata' );
-Route::get ( '/opinion', 'OpinionController@index' )->name ( 'opinion' );
+
+Route::get ( '/logindata', 'LogindataController@index' )->name ( 'logindata' )->middleware('role');
+
+Route::get ( '/opinion', 'OpinionController@index' )->name ( 'opinion' )->middleware('auth');
 Route::get ( '/opinion/{deleteno}','OpinionController@delete' );
-	//Route::get('/ajax/{deletecode}','UserdeleteController@delete');
