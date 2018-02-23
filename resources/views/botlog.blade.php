0aa2d80ed8b81472a2dac6f9a73265dfcc2ef043
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-23 11:23:10 +0900

    s

--- resources/views/botlog.blade.php
+++ resources/views/botlog.blade.php
@@ -39,36 +39,31 @@
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_id">No</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="dia_id"
-								value="{{$botlog->no}}" readonly>
+							<input type="text" class="form-control" id="dia_id" value="{{$botlog->no}}" readonly>
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_userid">ユーザーID</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="dia_userid"
-								value="{{$botlog->userid}}" readonly>
+							<input type="text" class="form-control" id="dia_userid" value="{{$botlog->userid}}" readonly>
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_time">日時</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="dia_time"
-								value="{{$botlog->time}}" readonly>
+							<input type="text" class="form-control" id="dia_time" value="{{$botlog->time}}" readonly>
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label" for="dia_opinion">質問</label>
+						<label class="col-sm-2 control-label" for="dia_contents">質問</label>
 						<div class="col-sm-10">
-							<textarea class="form-control" id="dia_opinion"
-								value="{{$botlog->contents}}" rows='5' readonly></textarea>
+							<textarea class="form-control" id="dia_contents" value="{{$botlog->contents}}" rows='5' readonly></textarea>
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label" for="dia_opinion">回答</label>
+						<label class="col-sm-2 control-label" for="dia_return">回答</label>
 						<div class="col-sm-10">
-							<textarea class="form-control" id="dia_opinion"
-								value="{{$botlog->return}}" rows='5' readonly></textarea>
+							<textarea class="form-control" id="dia_return" value="{{$botlog->return}}" rows='5' readonly></textarea>
 						</div>
 					</div>
 					@endforeach
