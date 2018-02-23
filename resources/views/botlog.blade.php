265b6831e89c3a2cdad638fa691d0945f62408dd
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-23 11:09:12 +0900

    s

--- resources/views/botlog.blade.php
+++ resources/views/botlog.blade.php
@@ -27,8 +27,80 @@
 	</tbody>
 </table>
 
+<div class="modal" id="shosaiDialog"  tabindex="-1">
+	<div class="modal-dialog">
+		<div class="modal-content" style="width:740px; margin-left: -20px;">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title" id="modal-label">詳細</h4>
+			</div>
+			<div class="modal-body">
+				<form class="form-horizontal">
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_id">ID</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="dia_id" value ="{{$opinion->id}}" readonly>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_userid">ユーザーID</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="dia_userid" value ="{{$opinion->userid}}" readonly>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_time">日時</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="dia_time" value ="{{$opinion->time}}" readonly>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_opinion">ご意見</label>
+						<div class="col-sm-10">
+							<textarea class="form-control" id="dia_opinion"  value = "{{$opinion->opinion}}" rows='5' readonly></textarea>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_sadness">悲しみ</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_sadness"  value = "{{$opinion->sadness}}" readonly>
+						</div>
+						<label class="col-sm-2 control-label" for="dia_joy">喜び</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_joy" value = "{{$opinion->joy}}" readonly>
+						</div>
+						<label class="col-sm-2 control-label" for="dia_fear">恐れ</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_fear" value = "{{$opinion->fear}}" readonly>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_disgust">嫌悪</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_disgust" value = "{{$opinion->disgust}}" readonly>
+						</div>
+						<label class="col-sm-2 control-label" for="dia_anger">怒り</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_anger" value = "{{$opinion->anger}}" readonly>
+						</div>
+					</div>
+				@endforeach
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button id="sback" type="button" class="btn btn-default" onclick="shosai_back()">＜＜前へ</button>
+				<button id="snext" type="button" class="btn btn-default" onclick="shosai_next()">次へ＞＞</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="container" align="center">
 	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
+<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog" value="モーダル表示" />
 </div>
 
 <script>
@@ -88,5 +160,45 @@
 					}
 				}
 			}
+			function detailwin(value){
+				document.getElementById("btn_modal").click();
+				for (var i = 0; i < dbvalue.length; i++){
+					if(dbvalue[i][0] == value){
+						shosai_idx = i;
+						modal_mod(i);
+					}
+				}
+			}
+			function shosai_back(){
+				shosai_idx = shosai_idx - 1;
+				modal_mod(shosai_idx);
+			}
+			function shosai_next(){
+				shosai_idx = shosai_idx + 1;
+				modal_mod(shosai_idx);
+			}
+			/*
+			function modal_mod(index){
+				document.getElementById('dia_id').value  = dbvalue[index][0];
+				document.getElementById('dia_userid').value  = dbvalue[index][1];
+				var idate = dbvalue[index][2].substr(0,4) + "/" + dbvalue[index][2].substr(4,2) + "/" + dbvalue[index][2].substr(6,2) + " " + dbvalue[index][2].substr(8,2) + ":" + dbvalue[index][2].substr(10,2);
+				document.getElementById('dia_time').value = idate;
+				document.getElementById('dia_sadness').value  = dbvalue[index][4];
+				document.getElementById('dia_joy').value  = dbvalue[index][5];
+				document.getElementById('dia_fear').value  = dbvalue[index][6];
+				document.getElementById('dia_disgust').value  = dbvalue[index][7];
+				document.getElementById('dia_anger').value  = dbvalue[index][8];
+				document.getElementById('dia_opinion').innerHTML  = dbvalue[index][3];
+				if(index == 0){
+					document.getElementById("sback").disabled = "true";
+				}else{
+					document.getElementById("sback").disabled = "";
+				}
+				if(index == dbvalue.length - 1){
+					document.getElementById("snext").disabled = "true";
+				}else{
+					document.getElementById("snext").disabled = "";
+				}
+			}*/
 </script>
 @endsection
