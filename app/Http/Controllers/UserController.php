fd63652242d209e7abb6e888cbeb109eeed5fbb9
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-02-27 14:59:00 +0900

    更新

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use App\User;
 
 class UserController
@@ -32,12 +33,26 @@ class UserController
 
 	public function update(Request $request)
 	{
-
+		/*
 		$deleteid = $request->deletecode;
 		$deleteuser = User::find($deleteid);
 		$deleteuser->delete();
+		*/
+		error_log("★★★★★★★★★★★★★update★★★★★★★★★★★★★★★");
 
 		return redirect('/users');
+	}
+
+	protected function validator(array $data) {
+		error_log("★★★★★★★★★★★★★validator★★★★★★★★★★★★★★★");
+		return Validator::make ( $data, [
+
+				'citycode' => 'required|string',
+				'name' => 'required|string|max:255',
+				'userid' => 'required|string|max:255|unique:users',
+				'organization' => 'required|string|max:255',
+				'password' => 'required|string|min:6|confirmed'
+		] );
 	}
 
 }
