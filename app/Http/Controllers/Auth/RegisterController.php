31529c4f6deeaee7d9b8cd930e5558c21664890a
sato taro <dsjvvknu@gmail.com>
2018-02-27 11:14:41 +0900

    k

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -75,7 +75,7 @@ class RegisterController extends Controller {
 				'userid' => $data ['userid'],
 				'organization' => $data ['organization'],
 				'password' => bcrypt ( $data ['password'] ),
-				'role' => $Authrole +1
+				'role' => 1
 
 		] );
 	}
