c95f62b6f1c4e482048013c4bcac8ea8734f5f6b
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-02-28 10:07:36 +0900

    n

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,7 @@ class UserController
 		$deleteuser->delete();
 		*/
 		$input = \Request::all();
-		error_log("★★★★★★★★★★★★★update★★★★★★★★★★★★★★★".count($input));
+		error_log("★★★★★★★★★★★★★update★★★★★★★★★★★★★★★".$input["name"]);
 
 		return redirect('/users');
 	}
