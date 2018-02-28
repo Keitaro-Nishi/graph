bd5024e899b105e56e078d42f97c3d0d0a4534c9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-28 14:03:54 +0900

    s

--- resources/views/facility.blade.php
+++ resources/views/facility.blade.php
@@ -113,7 +113,7 @@
 <div class="container" align="center">
 	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
 	<input id="btn_ins" type="button" class="btn btn-default" value="施設登録" onclick="insert()">
-	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog"/>
+	<input id="btn_modal" type="button" data-toggle="modal"  data-target="#shosaiDialog"/>
 </div>
 <script src="{{ asset('js/facility.js') }}"></script>
 
