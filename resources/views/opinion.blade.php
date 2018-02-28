46f19b1666cebf3e77d0b7d0dd0e2e80d796791e
sato taro <dsjvvknu@gmail.com>
2018-02-28 15:37:53 +0900

    k

--- resources/views/opinion.blade.php
+++ resources/views/opinion.blade.php
@@ -62,7 +62,7 @@
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_userid">ユーザーID</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="dia_userid" value ='' readonly>
+							<input type="text" class="form-control" id="dia_userid" readonly>
 						</div>
 					</div>
 					<div class="form-group">
