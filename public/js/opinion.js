ec57f70e6f89971da1cfcecef1e1d6bd41cf280d
sato taro <dsjvvknu@gmail.com>
2018-02-28 15:41:25 +0900

    l

--- public/js/opinion.js
+++ public/js/opinion.js
@@ -34,7 +34,7 @@ dbvalue = JSON.parse(opinion);
 
 			function drow() {
 
-				alert(dbvalue[0]["userid"]);
+				alert(dbvalue[0][2]);
 
 				if(rowIds.length == 0){
 					alert("削除する行を選択してください");
@@ -71,7 +71,6 @@ dbvalue = JSON.parse(opinion);
 					if(dbvalue[i][0] == value){
 						shosai_idx = i;
 						modal_mod(i);
-						alert(i);
 					}
 				}
 			}
