093ef529ff73bbab370f14ef0788c923a93f16c7
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-01 18:06:42 +0900

    s

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -56,6 +56,7 @@ class FacilityController
 		}else{
 			$facility->citycode= $cityCD;
 		}
+		/*
 		//名称
 		$facility->meisho= $input["meisho"];
 		//住所
@@ -76,7 +77,7 @@ class FacilityController
 		$facility->imageurl= $input["imageurl"];
 		//URL
 		$facility->genre3= $input["url"];
-
+*/
 		$facility = DB::insert('insert into facility (citycode, meisho, jusho, tel, genre1, genre2, genre3, lat, lng, imageurl, url, geom) values (?,?,?,?,?,?,?,?,?,?,?,?)',[00000,1,1,1,1,1,1,1,1,1,1,1]);
 
 		return redirect ( '/facility' );
