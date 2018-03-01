dff7fa1ac9aab5eba77f259b2b0a1eb7d38a307b
sato taro <dsjvvknu@gmail.com>
2018-03-01 18:18:20 +0900

    k

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -46,10 +46,6 @@
 	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog" value="モーダル表示" />
 </div>
 
-<!--
-<div>
-<input id="opinion" type= "text" value = '{{ $genregid2 }}'>
-</div> -->
 
 <div class="modal" id="shosaiDialog"  tabindex="-1">
 	<div class="modal-dialog">
