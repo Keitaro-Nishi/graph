b31e4bd356a30f1196c0aef051e8690fe5975680
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-01 18:09:10 +0900

    s

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Facility;
@@ -78,7 +78,7 @@ class FacilityController
 		//URL
 		$facility->genre3= $input["url"];
 */
-		$facility = Facility::insert('insert into facility (citycode, meisho, jusho, tel, genre1, genre2, genre3, lat, lng, imageurl, url, geom) values (?,?,?,?,?,?,?,?,?,?,?,?)',[00000,1,1,1,1,1,1,1,1,1,1,1]);
+		$facility = DB::insert('insert into facility (citycode, meisho, jusho, tel, genre1, genre2, genre3, lat, lng, imageurl, url, geom) values (?,?,?,?,?,?,?,?,?,?,?,?)',[00000,1,1,1,1,1,1,1,1,1,1,1]);
 
 		return redirect ( '/facility' );
 	}
