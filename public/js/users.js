efdb2747810a27e181c05ee7546df2fa47793a7e
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-01 17:50:55 +0900

    n

--- public/js/users.js
+++ public/js/users.js
@@ -7,7 +7,7 @@ $(function() {
 		columnSelection : false,
 		formatters: {
 	        "details": function($column, $row) {
-	        	return "<input type='button' class='btn btn-default' value='修正' onclick='detail(" $row.name + ",\"" + $row.userid + ",\"" + $row.organization + ",\"" + $row.citycode + ",\"" + $row.password + ")' > ";
+	        	return "<input type='button' class='btn btn-default' value='修正' onclick='detail(\"" $row.name + "\",\"" + $row.userid + "\",\"" + $row.organization + "\",\"" + $row.citycode + "\",\"" + $row.password + ")' > ";
              }
 	    }
 	}).on("selected.rs.jquery.bootgrid", function(e, rows) {
