5e2dbb4045a0c838ae0ed6839e9442e8e38f3558
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-01 10:31:19 +0900

    s

--- public/js/logimage.js
+++ public/js/logimage.js
@@ -7,12 +7,12 @@ $(function() {
 		keepSelection : true
 	}).on("selected.rs.jquery.bootgrid", function(e, rows) {
 		for (var i = 0; i < rows.length; i++) {
-			rowIds.push(rows[i].userid);
+			rowIds.push(rows[i].no);
 		}
 	}).on("deselected.rs.jquery.bootgrid", function(e, rows) {
 		for (var i = 0; i < rows.length; i++) {
 			rowIds.some(function(v, ii) {
-				if (v == rows[i].userid)
+				if (v == rows[i].no)
 					rowIds.splice(ii, 1);
 			});
 		}
