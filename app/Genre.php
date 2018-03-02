18d11a2a34e7e67df0f98e3f54a905ae4484b942
sato taro <dsjvvknu@gmail.com>
2018-03-02 15:05:34 +0900

    a

--- app/Genre.php
+++ app/Genre.php
@@ -10,7 +10,7 @@ class Genre extends Model
 
 	//use CompositePrimaryKeyTrait;
 
-	public $test = "テスト";
+	public $test = ["テスト1","テスト2","テスト3","テスト4"];
 	/*protected $fillable = [
 
 	'citycode', 'no', 'time', 'sex', 'age', 'opinion', 'sadness', 'joy', 'fear', 'disgust', 'anger', 'checked'
