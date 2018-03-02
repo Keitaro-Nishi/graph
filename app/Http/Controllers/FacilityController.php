24146810ef4c5002acf7554b4637ccddb151f10d
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-02 13:31:11 +0900

    s

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -71,8 +71,9 @@ class FacilityController
 		$imageurl = $input["imageurl"];
 		//URL
 		$url = $input["url"];
+
 		error_log("?????????????????".$meisho."?????????????????");
-		$result= DB::insert('insert into facility (citycode, id, meisho, jusho, tel, genre1, genre2, genre3, lat, lng, imageurl, url) values (?,?,?,?,?,?,?,?,?,?,?)',
+		$result= DB::insert('insert into facility (citycode, id, meisho, jusho, tel, genre1, genre2, genre3, lat, lng, imageurl, url) values (?,?,?,?,?,?,?,?,?,?,?,?)',
 				[$citycode, 2, $meisho, $jusho, $tel, $genre1, $genre2, 0, $lat, $lng, $imageurl, $url]);
 
 		if($result == "1"){
