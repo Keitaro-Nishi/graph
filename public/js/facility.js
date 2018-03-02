cec6ec4cfd168fd7d85f0078312bfd10ecd560e4
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-02 19:49:23 +0900

    s

--- public/js/facility.js
+++ public/js/facility.js
@@ -5,13 +5,12 @@ $(function() {
 		multiSelect : true,
 		keepSelection : true,
 		columnSelection : false,
-		/*
 	    formatters: {
 	        "mods": function($column, $row) {
 	        	return "<input type='button' class='btn btn-default' value='修正' onclick='modwin("  + $row.id + ",\"" + $row.meisho + "\",\"" + $row.jusho + "\",\"" + $row.tel + "\",\"" + $row.genre1 + "\",\"" + $row.genre2 + "\",\"" + $row.lat + "\",\"" + $row.lng + "\",\"" + $row.imageurl + "\",\"" + $row.url + "\")' > ";
              }
 	    }
-		 */
+
 	}).on("selected.rs.jquery.bootgrid", function(e, rows) {
 		for (var i = 0; i < rows.length; i++) {
 			rowIds.push(rows[i].id);
