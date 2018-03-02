71c70106b0fa127c8a846f645e1a4df26389a9cf
sato taro <dsjvvknu@gmail.com>
2018-03-02 09:48:52 +0900

    k

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -28,8 +28,8 @@ class GenreController
 			$genregid1 = DB::table('genre')->select('gid1')->get();
 			$array = (array)$genregid1;
 
-			error_log("★★★★★★★");
-			error_log($array[0]);
+			//error_log("★★★★★★★");
+			//error_log($array[0]);
 
 
 			for ($i = 0; $i< count($array); $i++) {
