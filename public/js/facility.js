672b87da76dab9a65041ec1d76f6ab86f5984623
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-02 19:52:36 +0900

    s

--- public/js/facility.js
+++ public/js/facility.js
@@ -76,21 +76,8 @@ function modwin(id,meisho,jusho,tel,genre1,genre2,lat,lng,imageurl,url){
 	document.getElementById('dia_meisho').value = meisho;
 	document.getElementById('dia_jusho').value = jusho;
 	document.getElementById('dia_tel').value = tel;
-	var options = document.getElementById('dia_genre1').options;
-	for(var i = 0; i < options.length; i++){
-		if(options[i].text === genre1){
-			options[i].selected = true;
-			break;
-		};
-	};
-	genre1change();
-	var options = document.getElementById('dia_genre2').options;
-	for(var i = 0; i < options.length; i++){
-		if(options[i].text === genre2){
-			options[i].selected = true;
-			break;
-		};
-	};
+	document.getElementById('dia_genre1').value = genre1;
+	document.getElementById('dia_genre2').value = genre2;
 	document.getElementById('dia_latlng').value = lat + "," + lng;
 	document.getElementById('dia_imageurl').value = imageurl;
 	document.getElementById('dia_url').value = url;
