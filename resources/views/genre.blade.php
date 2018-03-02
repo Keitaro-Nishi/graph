e29df2ca66160e82ad82a7f37e9d4b6c0836553d
sato taro <dsjvvknu@gmail.com>
2018-03-02 13:51:29 +0900

    k

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -21,7 +21,7 @@
 			@else
 			<td>小分類</td>
 			@endif
-			@for ($i = 0; $i < $genrearray.length; $i++)
+			@for ($i = 0; $i < 5; $i++)
 			<td>{{$genrearray[i]}}</td>
 			@endfor
 			@if($genre->bunrui == 1)
