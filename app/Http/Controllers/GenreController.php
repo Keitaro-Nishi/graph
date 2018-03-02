98537726e626b113459c8172e569f725e3a8eaf4
sato taro <dsjvvknu@gmail.com>
2018-03-02 09:37:42 +0900

    l

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -22,8 +22,8 @@ class GenreController
 		if($cityCD == "00000"){
 			$genres = Genre::all();
 		}else{
-			$genregid1 = array();
-			$genregid2 = array();
+			//$genregid1 = array();
+			//$genregid2 = array();
 			$genres= Genre::where('citycode', $cityCD)->orderBy('gid1', 'ASC')->orderBy('gid2', 'ASC')->get();
 			$genregid1 = DB::table('genre')->select('gid1')->get();
 
