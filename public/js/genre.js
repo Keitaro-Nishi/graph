2838bda5968863c3f8a8b27ec2b171e48938a3b0
sato taro <dsjvvknu@gmail.com>
2018-03-05 14:53:50 +0900

    k

--- public/js/genre.js
+++ public/js/genre.js
@@ -37,3 +37,59 @@ $(function() {
 	    }
 	});
 });
+
+
+function drow() {
+	//alert()
+
+	/*
+	if(rowIds.length == 0){
+		alert("削除する行を選択してください");
+		return;
+	}
+	//大分類が選択されている場合、小分類を削除する
+	var idarray = [];
+	var g1array = [];
+	for (var i = 0; i < rowIds.length; i++){
+		idarray.push(rowgid1[i] + "." + rowgid2[i]);
+		if(rowgid2[i] == "0"){
+			g1array.push(rowgid1[i]);
+		}
+	}
+	g1array.forEach(function(v, i){
+		for (var ii = idarray.length - 1; ii >= 0; ii--) {
+			var aos = idarray[ii].split(".");
+			if(aos[0] == v){
+				if(aos[1] > 0){
+					idarray.splice(ii,1);
+				}
+			}
+		}
+	});
+	var myRet = false;
+	if(g1array.length > 0){
+		myRet = confirm("選択行を削除しますか？\n※大分類を削除すると関連する小分類も削除されます");
+	}else{
+		myRet = confirm("選択行を削除しますか？");
+	}
+	if ( myRet == true ){
+		$.ajax({
+			type: "POST",
+			url: "genredel.php",
+			data: {
+				"id" : idarray
+			}
+		}).done(function (response) {
+			result = JSON.parse(response);
+			if(result == "OK"){
+				alert("削除しました");
+				location.reload();
+			}else{
+				alert("削除できませんでした");
+			}
+		}).fail(function () {
+			alert("削除できませんでした");
+		});
+	}
+	*/
+}
