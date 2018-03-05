8a8cdfb637650f697f6602742e2cb86f510688a3
sato taro <dsjvvknu@gmail.com>
2018-03-05 14:43:08 +0900

    a

--- public/js/genre.js
+++ public/js/genre.js
@@ -1,4 +1,5 @@
 var rowIds = [];
+var rowcitycode = [];
 var rowgid1 = [];
 var rowgid2 = [];
 var meishoOld = "";
@@ -13,16 +14,26 @@ $(function() {
 		columnSelection : false,
 	    keepSelection: true
 	}).on("selected.rs.jquery.bootgrid", function(e, rows) {
-		for (var i = 0; i < rows.length; i++) {
-			rowIds.push(rows[i].citycode);
-		}
-
+		for (var i = 0; i < rows.length; i++)
+	    {
+	        rowIds.push(rows[i].no);
+	        rowcitycode.push(rows[i].citycode);
+	        rowgid1.push(rows[i].gid1);
+	        rowgid2.push(rows[i].gid2);
+	        alert("rowIds:" + rows[i].no + "rowcitycode:" + rows[i].citycode + "rowgid1:" + rows[i].gid1 + " rowgid2:" + rows[i].gid2);
+	    }
 	}).on("deselected.rs.jquery.bootgrid", function(e, rows) {
-		for (var i = 0; i < rows.length; i++) {
-			rowIds.some(function(v, ii) {
-				if (v == rows[i].citycode)
-					rowIds.splice(ii, 1);
-			});
-		}
+		for (var i = 0; i < rows.length; i++)
+	    {
+	    	for (var ii = 0; ii < rowIds.length; ii++){
+		    	if(rowIds[ii] == rows[i].no){
+		    		rowIds.splice(ii,1);
+		    		rowcitycode.splice(ii,1);
+		    		rowgid1.splice(ii,1);
+		    		rowgid2.splice(ii,1);
+		    		break;
+		    	}
+	    	}
+	    }
 	});
 });
