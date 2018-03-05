d349a0f4362b9c85d6f4de6679d5d78288c9381d
sato taro <dsjvvknu@gmail.com>
2018-03-05 14:18:55 +0900

    j

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -5,7 +5,7 @@
 	class="table table-condensed table-hover table-striped">
 	<thead>
 		<tr>
-			   <th data-column-id='no' data-identifier='true' data-width='3%'>No</th>
+			   <th data-column-id='no' data-identifier='true' data-width='3%' data-visible="false"></th>
 			   @if (Auth::user()->citycode == "00000")
 			   <th data-column-id='citycode' data-width='20%' >市町村コード</th>
 			   @else
