5e1c5c4f05030bc0a63c36ea07ab954851c3755b
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-05 15:51:20 +0900

    s

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ Route::get ( '/', function () {
 
 Auth::routes ();
 
-Route::middleware ( [
-		'auth'
-] )->group ( function () {
+Route::middleware(['auth'])->group(function () {
 	Route::get ( '/register', 'Auth\RegisterController@showRegistrationForm' )->name ( 'register' )->middleware ( 'role' );
 
 	Route::get ( '/home', 'HomeController@index' )->name ( 'home' );
