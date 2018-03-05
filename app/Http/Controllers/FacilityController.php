55a62f349908ea5a7da2d84e19d5cd4c1804b7b6
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-05 13:23:09 +0900

    s

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -1,149 +1,112 @@
-<?php
+ <?php
 
-namespace App\Http\Controllers;
+	namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use App\Genre;
-use App\Facility;
+	use Illuminate\Http\Request;
+	use Illuminate\Http\Response;
+	use Illuminate\Support\Facades\DB;
+	use Illuminate\Support\Facades\Auth;
+	use Illuminate\Support\Facades\Validator;
+	use App\Facility;
 
-class FacilityController {
-	public function index(Request $request) {
-		$cityCD = Auth::user ()->citycode;
+	class FacilityController {
+		public function index(Request $request) {
+			$cityCD = Auth::user ()->citycode;
 
-		//if ($cityCD = "00000") {
-			$facilities = Facility::all (); /* ->orderBy('citycode', 'ASC') */
+			if ($cityCD = "00000") {
+				$facilities = Facility::all ();
+			} else {
+				$facilities = Facility::where ( 'citycode', $cityCD )->get ();
+			}
+			return view ( 'facility', [
+					'facilities' => $facilities
+			] );
+		}
+		public function update(Request $request) {
+			$input = \Request::all ();
+			error_log ( "?????????????????" . $input ["meisho"] . "?????????????????" );
 
-			foreach ( $facilities as $facility ) {
+			$rules = [
+					'meisho' => 'string|max:255',
+					'jusho' => 'string|max:255',
+					'tel' => 'string|max:14',
+					'genre1' => 'integer',
+					'genre2' => 'integer',
+					'genre3' => 'integer',
+					'imageurl' => 'string',
+					'url' => 'string'
+			];
 
-				$citycode = $facility->citycode;
-				$meisho = $facility->meisho;
-				$jusho = $facility->jusho;
-				$tel = $facility->tel;
-				$genreL;
-				$genreM;
-				$lat = $facility->lat;
-				$lng = $facility->lng;
-				$imageurl = $facility->imageurl;
-				$url = $facility->url;
+			$validator = Validator::make ( $input, $rules );
 
-				$genre1 = $facility->genre1;
-				$genre2 = $facility->genre2;
+			if ($validator->fails ()) {
+				return $validator->errors ();
+			}
 
-				error_log("☆☆☆☆☆☆☆☆",$lat,"☆☆☆☆☆☆☆☆");
+			// insert
+			$id = $input ["id"];
+			// 市町村コード
+			$citycode = Auth::user ()->citycode;
+			// 名称
+			$meisho = $input ["meisho"];
+			// 住所
+			$jusho = $input ["jusho"];
+			// 電話番号
+			$tel = $input ["tel"];
+			// ジャンル1
+			$genre1 = $input ["genre1"];
+			// ジャンル2
+			$genre2 = $input ["genre2"];
+			// ジャンル3
+			$genre3 = 0;
+			// 経度
+			$lat = $input ["lat"];
+			// 緯度
+			$lng = $input ["lng"];
+			// 画像URL
+			$imageurl = $input ["imageurl"];
+			// URL
+			$url = $input ["url"];
 
-				$bunruiL = DB::table ( 'genre' )->select ( 'meisho' )->where ( 'bunrui', 1 )->where ( 'gid1', $genre1 )->first ();
-				$genreL = $bunruiL;
-				$bunruiM = DB::table ( 'genre' )->select ( 'meisho' )->where ( 'bunrui', 2 )->where ( 'gid2', $genre2 )->first ();
-				$genreM = $bunruiM;
-
-				//error_log("☆☆☆☆☆☆☆☆",$genreL,"☆☆☆☆☆☆☆☆");
-
-				$facilitylist = [
+			error_log ( "?????????????????" . $citycode . "?????????????????" );
+			if ($input ["id"] == null) {
+				$result = DB::table ( 'facility' )->insertGetId ( [
 						'citycode' => $citycode,
 						'meisho' => $meisho,
 						'jusho' => $jusho,
 						'tel' => $tel,
-						'genreL' => $genreL,
-						'genreM' => $genreM,
+						'genre1' => $genre1,
+						'genre2' => $genre2,
+						'genre3' => $genre3,
 						'lat' => $lat,
 						'lng' => $lng,
 						'imageurl' => $imageurl,
-						'url' => $url
-				];
-				array_push ( $facilitylists, $facilitylist );
+						'url' => $url,
+						'geom' => \DB::raw ( "public.ST_GeomFromText('POINT({$lat} {$lng})',4326)" )
+				] );
+			} else {
+				error_log ( "?????????????????" . $id . "?????????????????" );
+				$result = DB::table ( 'facility' )->where ( 'id', $id )->update ( [
+						'citycode' => $citycode,
+						'meisho' => $meisho,
+						'jusho' => $jusho,
+						'tel' => $tel,
+						'genre1' => $genre1,
+						'genre2' => $genre2,
+						'genre3' => $genre3,
+						'lat' => $lat,
+						'lng' => $lng,
+						'imageurl' => $imageurl,
+						'url' => $url,
+						'geom' => \DB::raw ( "public.ST_GeomFromText('POINT({$lat} {$lng})',4326)" )
+				] );
 			}
-		//} else {
-			//$facilities = Facility::where ( 'citycode', $cityCD )->get ();
-		//}
-		return view ( 'facility', compact ( 'facilitylists' ) );
-	}
-	public function update(Request $request) {
-		$input = \Request::all ();
-
-		$rules = [
-				'meisho' => 'string|max:255',
-				'jusho' => 'string|max:255',
-				'tel' => 'string|max:14',
-				'genre1' => 'integer',
-				'genre2' => 'integer',
-				'genre3' => 'integer',
-				'imageurl' => 'string',
-				'url' => 'string'
-		];
-
-		$validator = Validator::make ( $input, $rules );
-
-		if ($validator->fails ()) {
-			return $validator->errors ();
+			error_log ( "?????????????????" . $result . "?????????????????" );
 		}
-
-		// insert
-		$id = $input ["id"];
-		// 市町村コード
-		$citycode = Auth::user ()->citycode;
-		// 名称
-		$meisho = $input ["meisho"];
-		// 住所
-		$jusho = $input ["jusho"];
-		// 電話番号
-		$tel = $input ["tel"];
-		// ジャンル1
-		$genre1 = $input ["genre1"];
-		// ジャンル2
-		$genre2 = $input ["genre2"];
-		// ジャンル3
-		$genre3 = 0;
-		// 経度
-		$lat = $input ["lat"];
-		// 緯度
-		$lng = $input ["lng"];
-		// 画像URL
-		$imageurl = $input ["imageurl"];
-		// URL
-		$url = $input ["url"];
-
-		if ($input ["id"] == null) {
-			// 新規登録
-			$result = DB::table ( 'facility' )->insertGetId ( [
-					'citycode' => $citycode,
-					'meisho' => $meisho,
-					'jusho' => $jusho,
-					'tel' => $tel,
-					'genre1' => $genre1,
-					'genre2' => $genre2,
-					'genre3' => $genre3,
-					'lat' => $lat,
-					'lng' => $lng,
-					'imageurl' => $imageurl,
-					'url' => $url,
-					'geom' => \DB::raw ( "public.ST_GeomFromText('POINT({$lat} {$lng})',4326)" )
-			] );
-		} else {
-			// 編集
-			$result = DB::table ( 'facility' )->where ( 'id', $id )->update ( [
-					'citycode' => $citycode,
-					'meisho' => $meisho,
-					'jusho' => $jusho,
-					'tel' => $tel,
-					'genre1' => $genre1,
-					'genre2' => $genre2,
-					'genre3' => $genre3,
-					'lat' => $lat,
-					'lng' => $lng,
-					'imageurl' => $imageurl,
-					'url' => $url,
-					'geom' => \DB::raw ( "public.ST_GeomFromText('POINT({$lat} {$lng})',4326)" )
-			] );
+		public function delete(Request $request) {
+			$deleteid = $request->deleteid;
+			$deletefacility = Facility::find ( $deleteid );
+			$deletefacility->delete ();
+			return redirect ( '/facility' );
 		}
 	}
-	public function delete(Request $request) {
-		$deleteid = $request->deleteid;
-		$deletefacility = Facility::find ( $deleteid );
-		$deletefacility->delete ();
-		return redirect ( '/facility' );
-	}
-}
--- resources/views/facility.blade.php
+++ resources/views/facility.blade.php
@@ -1,50 +1,47 @@
 @extends('layouts.app')
 
-@section('title')
-施設管理
-@stop
+@section('title') 施設管理 @stop
 
 @section('content')
-<table id="grid-basic" class="table table-condensed table-hover table-striped">
+<table id="grid-basic"
+	class="table table-condensed table-hover table-striped">
 	<thead>
 		<tr>
 			<th data-column-id="id" data-identifier="true" data-visible="false">ID</th>
 			<th data-column-id="meisho">名称</th>
 			<th data-column-id="jusho">住所</th>
 			<th data-column-id="tel">電話番号</th>
-			<th data-column-id="genreL">ジャンル１</th>
-			<th data-column-id="genreM">ジャンル2</th>
+			<th data-column-id="genre1">ジャンル１</th>
+			<th data-column-id="genre2">ジャンル2</th>
 			<th data-column-id="lat">緯度</th>
 			<th data-column-id="lng">経度</th>
 			<th data-column-id="imageurl">画像URL</th>
 			<th data-column-id="url">詳細URL</th>
-			<th data-column-id='detail' data-formatter='mods' data-sortable='false'></th>
+			<th data-column-id='detail' data-formatter='mods'
+				data-sortable='false'></th>
 		</tr>
 	</thead>
 	<tbody>
-		@foreach($facilitylists as $facilitylist)
+		@foreach($facilities as $facility)
 		<tr>
-			<td>{{$facilitylist['id']}}</td>
-			@if (Auth::user()->citycode == "00000")
-			<td>{{$facilitylist['citycode']}}</td>
-			@endif
-			<td>{{$facilitylist['meisho']}}</td>
-			<td>{{$facilitylist['jusho']}}</td>
-			<td>{{$facilitylist['tel']}}</td>
-			<td>{{$facilitylist['genreL']}}</td>
-			<td>{{$facilitylist['genreM']}}</td>
-			<td>{{$facilitylist['lat']}}</td>
-			<td>{{$facilitylist['lng']}}</td>
-			<td>{{$facilitylist['imageurl']}}</td>
-			<td>{{$facilitylist['url']}}</td>
+			<td>{{$facility->id}}</td>
+			<td>{{$facility->meisho}}</td>
+			<td>{{$facility->jusho}}</td>
+			<td>{{$facility->tel}}</td>
+			<td>{{$facility->genre1}}</td>
+			<td>{{$facility->genre2}}</td>
+			<td>{{$facility->lat}}</td>
+			<td>{{$facility->lng}}</td>
+			<td>{{$facility->imageurl}}</td>
+			<td>{{$facility->url}}</td>
 			<td></td>
 		</tr>
 		@endforeach
 	</tbody>
 </table>
-<div class="modal" id="shosaiDialog"  tabindex="-1">
+<div class="modal" id="shosaiDialog" tabindex="-1">
 	<div class="modal-dialog">
-		<div class="modal-content" style="width:740px; margin-left: -20px;">
+		<div class="modal-content" style="width: 740px; margin-left: -20px;">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">
 					<span aria-hidden="true">&times;</span>
@@ -53,7 +50,7 @@
 			</div>
 			<div class="modal-body">
 				<form class="form-horizontal">
-					<div class="form-group"  style="display:none">
+					<div class="form-group" style="display: none">
 						<label class="col-sm-2 control-label" for="dia_id">id</label>
 						<div class="col-sm-10">
 							<input id="dia_id" class="form-control" name="id" value="">
@@ -62,19 +59,22 @@
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_meisho">施設名称</label>
 						<div class="col-sm-10">
-							<input id="dia_meisho" class="form-control" maxlength="40" name="meisho" value="" placeholder="行政公園">
+							<input id="dia_meisho" class="form-control" maxlength="40"
+								name="meisho" value="" placeholder="行政公園">
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_jusho">住所</label>
 						<div class="col-sm-10">
-							<input id="dia_jusho" class="form-control" maxlength="128" name="jusho" value="" placeholder="行政市行政1-1-1">
+							<input id="dia_jusho" class="form-control" maxlength="128"
+								name="jusho" value="" placeholder="行政市行政1-1-1">
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_tel">電話番号</label>
 						<div class="col-sm-10">
-							<input id="dia_tel" class="form-control" type="tel" name="tel" value="" maxlength="14" placeholder="000-000-0000">
+							<input id="dia_tel" class="form-control" type="tel" name="tel"
+								value="" maxlength="14" placeholder="000-000-0000">
 						</div>
 					</div>
 					<div class="form-group">
@@ -96,28 +96,35 @@
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_latlng">緯度・経度</label>
 						<div class="col-sm-10">
-							<input id="dia_latlng" class="form-control" maxlength="33" name="latlng" value="" placeholder="999.99999,999.99999">
-							<input type="button" class="btn btn-default" style="display:inline;" onclick="map()" value="地図の確認" style="width: 100px;"/>
+							<input id="dia_latlng" class="form-control" maxlength="33"
+								name="latlng" value="" placeholder="999.99999,999.99999"> <input
+								type="button" class="btn btn-default" style="display: inline;"
+								onclick="map()" value="地図の確認" style="width: 100px;" />
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_imageurl">画像ＵＲＬ</label>
 						<div class="col-sm-10">
-							<input id="dia_imageurl" class="form-control" name="imageurl" value="" maxlength="300" placeholder="https://www.yyy.zzz.jpg">
-							<input type="button" class="btn btn-default" style="display:inline; width: 100px;" onclick="image()" value="画像の確認"/>
-							※必ずhttpsから始まるURLを指定してください
+							<input id="dia_imageurl" class="form-control" name="imageurl"
+								value="" maxlength="300" placeholder="https://www.yyy.zzz.jpg">
+							<input type="button" class="btn btn-default"
+								style="display: inline; width: 100px;" onclick="image()"
+								value="画像の確認" /> ※必ずhttpsから始まるURLを指定してください
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_url">詳細ＵＲＬ</label>
 						<div class="col-sm-10">
-							<input id="dia_url" class="form-control" maxlength="300" name="url" value="" placeholder="http://www.yyy.zzz.html">
+							<input id="dia_url" class="form-control" maxlength="300"
+								name="url" value="" placeholder="http://www.yyy.zzz.html">
 						</div>
 					</div>
-					<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-					<div class="text-right" >
+					<input type="hidden" id="_token" name="_token"
+						value="{{ csrf_token() }}">
+					<div class="text-right">
 						<button type="button" class="btn btn-primary" onclick="update()">登録</button>
-						<button id="dia_close" type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+						<button id="dia_close" type="button" class="btn btn-default"
+							data-dismiss="modal">閉じる</button>
 					</div>
 				</form>
 			</div>
@@ -126,13 +133,16 @@
 </div>
 @if (Auth::user()->citycode != 00000)
 <div class="container" align="center">
-	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
-	<input id="btn_ins" type="button" class="btn btn-default" value="施設登録" onclick="insert()">
-	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog"/>
+	<input id="btn_del" type="button" class="btn btn-default"
+		value="選択行の削除" onclick="drow()"> <input id="btn_ins" type="button"
+		class="btn btn-default" value="施設登録" onclick="insert()"> <input
+		id="btn_modal" type="button" style="display: none" data-toggle="modal"
+		data-target="#shosaiDialog" />
 </div>
 @else
 <div class="container" align="center">
-	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
+	<input id="btn_del" type="button" class="btn btn-default"
+		value="選択行の削除" onclick="drow()">
 </div>
 @endif
 <script src="{{ asset('js/facility.js') }}"></script>
