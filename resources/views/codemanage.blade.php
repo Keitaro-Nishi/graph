4697c46a1d71029b0fa2121c5bd3d1bf75146991
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-05 15:56:49 +0900

    n

--- resources/views/codemanage.blade.php
+++ resources/views/codemanage.blade.php
@@ -6,13 +6,13 @@
 
 @section('content')
 <div class="container">
+<select class="form-control" id="codesel" onChange="codeselChange()" width="200">
+		@foreach($bunrui as $value)
+			<option value={{$value->code2}} selected>{{$value->meisho}}</option>
+		@endforeach
+</select>
 <table id="grid-basic"
 	class="table table-condensed table-hover table-striped">
-	<select class="form-control" id="codesel" onChange="codeselChange()" width="200">
-		@foreach($bunrui as $value)
-			<option value={{$code->code2}} selected>{{$value->meisho}}</option>
-		@endforeach
-	</select>
 	<thead>
 		<tr>
 			<th data-column-id="code12" data-identifier="true" data-visible="false">コード12</th>
@@ -43,7 +43,7 @@
 
 <div class="container" align="center">
 	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
-	<input id="btn_ins" type="button" class="btn btn-default" value="ユーザー登録" onclick="insert()">
+	<input id="btn_ins" type="button" class="btn btn-default" value="コード追加" onclick="insert()">
 	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog"/>
 </div>
 <script src="{{ asset('js/codemanage.js') }}"></script>
