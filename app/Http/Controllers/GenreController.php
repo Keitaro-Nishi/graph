28445ef3e43b24d1227912bb490f80f04123c98b
sato taro <dsjvvknu@gmail.com>
2018-03-06 15:15:38 +0900

    a

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -142,10 +142,10 @@ class GenreController
 				*/
 
 				DB::table('genre')->insert(['citycode'=> $cityCD,'bunrui' => $bunrui,'gid1' => $gid1,'gid2' => $gid2,'gid3' =>0,'meisho' => $meisho]);
-
+				return \Response::json(['status' => 'OK']);
 			}
 		}
-		return \Response::json(['status' => 'OK']);
+
 	}
 
 	public function init(Request $request)
