575a2a63265b7d5a0799b62dbc3b8b2485a00f2f
sato taro <dsjvvknu@gmail.com>
2018-03-06 13:35:06 +0900

    a

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -116,6 +116,12 @@ class GenreController
 				error_log("○○○○○○");
 				error_log($gid1);
 				DB::table('genre')->insert(['bunrui' =>$bunrui, 'gid1' => $gid1,'gid2' =>0,'gid3' =>0,'meisho' =>$meisho]);
+			}else{
+				$gid2data= DB::table('genre')->select('gid2')->where('gid1',$gid1)->orderBy('gid2', 'DESC')->get();
+				$gid2 = $gid2data[0] + 1;
+				error_log("●●●●●●●");
+				error_log($gid2);
+				DB::table('genre')->insert(['bunrui' =>$bunrui, 'gid1' => $gid1,'gid2' =>$gid2,'gid3' =>0,'meisho' =>$meisho]);
 			}
 		}
 	}
