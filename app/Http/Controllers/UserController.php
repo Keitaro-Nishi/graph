36e9457dc6a486669f46ffe7c246e52e4ea6116e
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-07 16:40:06 +0900

    n

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController
 			$users = User::where('role', '<' ,(int)2)->get();
 		}else{
 			//$users= User::where('citycode', $cityCD)->get();
-			$users = User::select()->where('citycode', $cityCD)->leftJoin('code', function ($join) {
+			$users = User::select()->where('users.citycode', $cityCD)->leftJoin('code', function ($join) {
 				$organizationCD = (int)12;
 				$join->on('users.citycode', '=', 'code.citycode')->where('code.code1', $organizationCD);
 				$join->on('users.organization', '=', 'code.code2');
