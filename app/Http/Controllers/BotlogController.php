45c18c9c80a1a1432f570930b0e6b1bba76f9fde
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-07 11:18:16 +0900

    s

--- app/Http/Controllers/BotlogController.php
+++ app/Http/Controllers/BotlogController.php
@@ -22,12 +22,28 @@ class BotlogController
 		return view('botlog',['botlogs'=>$botlogs]);
 	}
 
+	public function request() {
+		$this->requestall = \Request::all ();
+		if ($this->requestall ["param"] == "update") {
+			return $this->update ();
+		} elseif ($this->requestall ["param"] == "delete") {
+			return $this->delete ();
+		} else {
+			return \Response::json ( [
+					'status' => 'NG'
+			] );
+		}
+	}
+
 	public function delete(Request $request)
 	{
-
-		$deleteno = $request->deleteno;
-		$deletebotlog = Botlog::find($deleteno);
-		$deletebotlog->delete();
+		$input = $this->requestall;
+		$nos = $input ["nos"];
+		$cityCD = Auth::user ()->citycode;
+		foreach ( $nos as $no ) {
+			DB::table('facility')->where('id',$no)->delete();
+		}
+		return \Response::json(['status' => 'OK']);
 
 		return redirect('/botlog');
 	}
