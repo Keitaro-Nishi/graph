c864c4c8c491c76e4b1db2ca42b66da9bbfdd54e
sato taro <dsjvvknu@gmail.com>
2018-03-07 13:32:46 +0900

    a

--- app/Http/Controllers/GenreintController.php
+++ app/Http/Controllers/GenreintController.php
@@ -28,8 +28,11 @@ class GenreintController
 
 	public function intentSearch()
 	{
-		$input = $this->requestall;
+		$workspace_id = getenv('CVS_WORKSPASE_ID');
+		$username = getenv('CVS_USERNAME');
+		$password = getenv('CVS_PASS');
 
+		$input = $this->requestall;
 		$param = $_POST['param'];
 		$g1meisho= $_POST['g1meisho'];
 		$g2meisho= $_POST['g2meisho'];
@@ -38,7 +41,31 @@ class GenreintController
 		$data = "";
 		error_log("★★★★★★★★★★★★★★★★★★g1meisho:".$g1meisho." g2meisho:".$g2meisho." param:".$param." sword:".$sword);
 
+		global $url,$g1meisho,$workspace_id;
+		$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents/".$g1meisho."/examples?version=2017-05-26&export=true";
+		$jsonString = callWatson2();
+		$json = json_decode($jsonString, true);
+		$arr = array();
+		foreach ($json["examples"] as $value){
+			array_push($arr,$value["text"]);
+		}
+		echo json_encode($arr);
 
+	}
+
+	function callWatson2(){
+		global $curl, $url, $username, $password, $data, $options;
+		$curl = curl_init($url);
+		$options = array(
+				CURLOPT_HTTPHEADER => array(
+						'Content-Type: application/json',
+				),
+				CURLOPT_USERPWD => $username . ':' . $password,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_RETURNTRANSFER => true,
+		);
+		curl_setopt_array($curl, $options);
+		return curl_exec($curl);
 	}
 
 
