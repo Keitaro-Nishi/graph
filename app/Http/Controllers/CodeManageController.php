8f460d600dff1c655076602fb596dbe05821d0cd
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-07 13:03:51 +0900

    n

--- app/Http/Controllers/CodeManageController.php
+++ app/Http/Controllers/CodeManageController.php
@@ -17,7 +17,7 @@ class CodeManageController
 		if(Auth::user()->role == (int)0){
 			$codes= Code::orderBy('citycode', 'ASC')->orderBy('code1', 'ASC')->orderBy('code2', 'ASC')->get();
 			$bunrui = Code::where('citycode', '00000')->where('code1', (int)0)->orderBy('code2', 'ASC')->get();
-			$citycodes = Code::groupBy('citycode')->orderBy('citycode', 'ASC')->get();
+			$citycodes = Code::distinct()->select('citycode')->get();
 		}else{
 			$codes= Code::where('citycode', $cityCD)->where('class2', '<>', '1')->orderBy('code1', 'ASC')->orderBy('code2', 'ASC')->get();
 			$bunrui = Code::where('citycode', '00000')->where('code1', (int)0)->where('code2', '>' ,(int)0)->where('class2', '<>', '1')->orderBy('code2', 'ASC')->get();
