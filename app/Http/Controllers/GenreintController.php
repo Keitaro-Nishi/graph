a63cd6e992159441128aa18146a8984a2f897d92
sato taro <dsjvvknu@gmail.com>
2018-03-08 09:13:31 +0900

    l

--- app/Http/Controllers/GenreintController.php
+++ app/Http/Controllers/GenreintController.php
@@ -46,32 +46,19 @@ class GenreintController
 		//error_log("☆☆☆☆☆☆☆");
 		//error_log("g1meisho:".$g1meisho." g2meisho:".$g2meisho." param:".$param." sword:".$sword);
 
-		error_log("●●●●●●");
 		$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents/".$g1meisho."/examples?version=2017-05-26&export=true";
-		error_log("!!!!!!!");
 		$jsonString = $this->callWatson2();
-		error_log("★★★★★★★");
 		$json = json_decode($jsonString, true);
-		error_log("☆☆☆☆☆☆☆");
 		$arr = array();
 		foreach ($json["examples"] as $value){
 			array_push($arr,$value["text"]);
 		}
+		error_log("●●●●●●");
 		echo json_encode($arr);
 	}
 
 	public function callWatson2(){
 		global $curl, $url, $username, $password, $data, $options;
-
-		/*$workspace_id = getenv('CVS_WORKSPASE_ID');
-		$username = getenv('CVS_USERNAME');
-		$password = getenv('CVS_PASS');
-		*/
-
-		error_log("○○○○○○");
-		error_log($username);
-		error_log($password);
-		error_log($url);
 
 		$curl = curl_init($url);
 		$options = array(
@@ -83,6 +70,7 @@ class GenreintController
 				CURLOPT_RETURNTRANSFER => true,
 		);
 		curl_setopt_array($curl, $options);
+		error_log("★★★★★★");
 		return curl_exec($curl);
 	}
 
