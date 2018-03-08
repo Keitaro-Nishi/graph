68e18e1e05b41d84c620c13f32c2c6203a7f246d
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-08 12:34:41 +0900

    n

---
+++ public/js/parameter.js
@@ -0,0 +1,65 @@
+var select_val = 0;
+
+//$(function() {
+function init() {
+	document.getElementById('messid').selectedIndex = 0;
+	messidChange();
+}
+
+function messidChange(){
+	select_val = document.getElementById('messid').value;
+	var messfind = false;
+
+	//メッセージ検索
+	for(var i=0; i < messages.length; i++){
+		if(messages[i]['id'] == select_val){
+			document.getElementById('mess').value = messages[i]['message'];
+			messfind = true;
+			break;
+		}
+	}
+
+	if(!messfind){
+		document.getElementById('mess').value = "";
+	}
+}
+
+function update(){
+	var message = document.getElementById('mess').value;
+	var _token = document.getElementById('_token').value;
+	$.ajax({
+		type: "POST",
+		dataType: "JSON",
+		data: {
+			"param" : "update",
+			"id" : select_val,
+			"message" : message,
+			"_token" : _token
+		}
+	}).done(function (response) {
+		if(response.status == "OK"){
+			bootbox.alert({
+				message: "更新しました",
+				size: 'small',
+				callback: function () {
+					for(var i=0; i < messages.length; i++){
+						if(messages[i]['id'] == select_val){
+							messages[i]['message'] = message;
+							break;
+						}
+					}
+				}
+			});
+		}else{
+			bootbox.alert({
+				message: "更新できませんでした",
+				size: 'small'
+			});
+		}
+    }).fail(function () {
+    	bootbox.alert({
+			message: "更新できませんでした",
+			size: 'small'
+		});
+    });
+}
