b8e9ba73f272bf8647affdf02dcffee7e35b5365
sato taro <dsjvvknu@gmail.com>
2018-03-09 16:26:33 +0900

    a

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Genre;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use App\Libs\callwatson;
+use app\Libs\callwatson;
 
 
 class GenreController
