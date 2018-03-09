b088031ea5445d70c194b55bc29c86f71dc138df
sato taro <dsjvvknu@gmail.com>
2018-03-09 11:53:50 +0900

    l

--- app/watson/callwatson.php
+++ app/watson/callwatson.php
@@ -122,7 +122,7 @@ class callwatson{
 		}
 	}
 
-	public static function callWatson(){
+	public function callWatson(){
 		//global $curl, $url, $username, $password, $data, $options;
 		error_log("★★★★★★★");
 
