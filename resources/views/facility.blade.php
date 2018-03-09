952e0b26d3698a6f885665222eb96e10218df9eb
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-09 13:11:32 +0900

    s

--- resources/views/facility.blade.php
+++ resources/views/facility.blade.php
@@ -83,7 +83,7 @@
 						<div class="col-sm-10">
 							<select class="form-control" id="dia_genre1" name="organization">
 								@foreach($facilities as $value)
-									<option value="{{$value->gid1}}" selected>{{$value->meisho1}}</option>
+									<option value="{{$value->genre1}}" selected>{{$value->meisho1}}</option>
 								@endforeach
 							</select>
 						</div>
