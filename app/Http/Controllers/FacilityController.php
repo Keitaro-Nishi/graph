14a127806d2c40eb38b76413a3dfcf17bbc826fa
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-12 10:35:37 +0900

    s

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -42,8 +42,8 @@ class FacilityController {
 		error_log("???????????????????42".$facilities[0]->meisho1);
 		$larges= Genre::where('citycode', $cityCD)->where('bunrui', 1)->orderBy('gid1', 'ASC')->get();
 		$mediums= Genre::where('citycode', $cityCD)->where('bunrui', 2)->orderBy('gid2', 'ASC')->get();
-		return view('facility',['facilities' => $facilities, 'larges'=>$larges, 'mediums'=>$mediums]);
 		error_log("???????????????????46".$mediums[0]->meisho);
+		return view('facility',['facilities' => $facilities, 'larges'=>$larges, 'mediums'=>$mediums]);
 	}
 
 	public function request() {
