e85656615767b5fdc81a18052e1cf7e39c6a113f
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-12 18:56:52 +0900

    s

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -42,10 +42,10 @@ class FacilityController {
 		foreach ($genre1value as $j1value){
 			$gid1 = $j1value->gid1;
 			$j2value= Genre::select('gid2', 'meisho')->where( 'citycode', $cityCD )->where('gid1', $gid1)->get();
-			//error_log ( print_r($j2value, true));
+			error_log ( print_r($j2value, true));
 			$genre2value = array($gid1 => $j2value);
 		}
-		error_log ( print_r($genre2value,true));
+		error_log ( print_r($genre2value, true));
 
 		return view ( 'facility', [
 				'facilities' => $facilities,
