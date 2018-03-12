fa61cc716c10636abb5539a0b1db4ec93a7a4b1f
sato taro <dsjvvknu@gmail.com>
2018-03-12 16:16:21 +0900

    あ

--- app/Http/Controllers/GenreintController.php
+++ app/Http/Controllers/GenreintController.php
@@ -55,11 +55,13 @@ class GenreintController
 		$jsonString = $watson->callWatson2($url,$username,$password);
 		$json = json_decode($jsonString, true);
 
-		error_log("★★★★★");
-		error_log(print_r($json,true));
+		//error_log("★★★★★");
+		//error_log(print_r($json,true));
 
 		$arr = array();
 		foreach ($json["examples"] as $value){
+			error_log("●●●●");
+			error_log($value["text"]);
 			array_push($arr,$value["text"]);
 		}
 
