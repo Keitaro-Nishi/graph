3a494464dc64d1e2429e61d64f87aa851aa2a594
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-12 12:00:16 +0900

    s

--- resources/views/facility.blade.php
+++ resources/views/facility.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
-@section('title') 施設管理
 
+@section('title')
+施設管理
 @stop
+
 @section('content')
-<table id="grid-basic"
-	class="table table-condensed table-hover table-striped">
+<table id="grid-basic" class="table table-condensed table-hover table-striped">
 	<thead>
 		<tr>
 			<th data-column-id="id" data-identifier="true" data-visible="false">ID</th>
@@ -20,8 +21,7 @@
 			<th data-column-id="imageurl">画像URL</th>
 			<th data-column-id="url">詳細URL</th>
 			<th data-column-id="citycode" data-visible="false"></th>
-			<th data-column-id='detail' data-formatter='mods'
-				data-sortable='false'></th>
+			<th data-column-id='detail' data-formatter='mods' data-sortable='false'></th>
 		</tr>
 	</thead>
 	<tbody>
@@ -45,9 +45,9 @@
 		@endforeach
 	</tbody>
 </table>
-<div class="modal" id="shosaiDialog" tabindex="-1">
+<div class="modal" id="shosaiDialog"  tabindex="-1">
 	<div class="modal-dialog">
-		<div class="modal-content" style="width: 740px; margin-left: -20px;">
+		<div class="modal-content" style="width:740px; margin-left: -20px;">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">
 					<span aria-hidden="true">&times;</span>
@@ -56,7 +56,7 @@
 			</div>
 			<div class="modal-body">
 				<form class="form-horizontal">
-					<div class="form-group" style="display: none">
+					<div class="form-group"  style="display:none">
 						<label class="col-sm-2 control-label" for="dia_id">id</label>
 						<div class="col-sm-10">
 							<input id="dia_id" class="form-control" name="id" value="">
@@ -65,30 +65,31 @@
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_meisho">施設名称</label>
 						<div class="col-sm-10">
-							<input id="dia_meisho" class="form-control" maxlength="40"
-								name="meisho" value="" placeholder="行政公園">
+							<input id="dia_meisho" class="form-control" maxlength="40" name="meisho" value="" placeholder="行政公園">
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_jusho">住所</label>
 						<div class="col-sm-10">
-							<input id="dia_jusho" class="form-control" maxlength="128"
-								name="jusho" value="" placeholder="行政市行政1-1-1">
+							<input id="dia_jusho" class="form-control" maxlength="128" name="jusho" value="" placeholder="行政市行政1-1-1">
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_tel">電話番号</label>
 						<div class="col-sm-10">
-							<input id="dia_tel" class="form-control" type="tel" name="tel"
-								value="" maxlength="14" placeholder="000-000-0000">
+							<input id="dia_tel" class="form-control" type="tel" name="tel" value="" maxlength="14" placeholder="000-000-0000">
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_genre1">ジャンル１</label>
 						<div class="col-sm-10">
-							<select class="form-control" id="dia_genre1" name="genre1">
+						<!--
+							<select class="form-control" id="dia_genre1"  onChange="genre1change()">
+							</select>
+						-->
+							<select class="form-control" id="dia_genre1" name="genre1" onChange="genre1change()">
 								@foreach($larges as $value)
-								<option value="{{$value->gid1}}" selected>{{$value->meisho}}</option>
+									<option value="{{$value->gid1}}" selected>{{$value->meisho}}</option>
 								@endforeach
 							</select>
 						</div>
@@ -96,62 +97,55 @@
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_genre2">ジャンル２</label>
 						<div class="col-sm-10">
-							<select class="form-control" id="dia_genre2" name="genre2">
-								@foreach($mediums as $value)
-								<option value="{{$value->gid2}}" selected>{{$value->meisho}}</option>
-								@endforeach
-								<select class="form-control" id="dia_genre2">
+							<select class="form-control" id="dia_genre2">
 							</select>
-
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_latlng">緯度・経度</label>
 						<div class="col-sm-10">
-							<input id="dia_latlng" class="form-control" maxlength="33"
-								name="latlng" value="" placeholder="999.99999,999.99999"> <input
-								type="button" class="btn btn-default" style="display: inline;"
-								onclick="map()" value="地図の確認" style="width: 100px;" />
+							<input id="dia_latlng" class="form-control" maxlength="33" name="latlng" value="" placeholder="999.99999,999.99999">
+							<input type="button" class="btn btn-default" style="display:inline;" onclick="map()" value="地図の確認" style="width: 100px;"/>
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_imageurl">画像ＵＲＬ</label>
 						<div class="col-sm-10">
-							<input id="dia_imageurl" class="form-control" name="imageurl"
-								value="" maxlength="300" placeholder="https://www.yyy.zzz.jpg">
-							<input type="button" class="btn btn-default"
-								style="display: inline; width: 100px;" onclick="image()"
-								value="画像の確認" /> ※必ずhttpsから始まるURLを指定してください
+							<input id="dia_imageurl" class="form-control" name="imageurl" value="" maxlength="300" placeholder="https://www.yyy.zzz.jpg">
+							<input type="button" class="btn btn-default" style="display:inline; width: 100px;" onclick="image()" value="画像の確認"/>
+							※必ずhttpsから始まるURLを指定してください
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="col-sm-2 control-label" for="dia_url">詳細ＵＲＬ</label>
 						<div class="col-sm-10">
-							<input id="dia_url" class="form-control" maxlength="300"
-								name="url" value="" placeholder="http://www.yyy.zzz.html">
+							<input id="dia_url" class="form-control" maxlength="300" name="url" value="" placeholder="http://www.yyy.zzz.html">
 						</div>
 					</div>
-					<input type="hidden" id="_token" name="_token"
-						value="{{ csrf_token() }}">
-					<div class="text-right">
+					<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
+					<div class="text-right" >
 						<button type="button" class="btn btn-primary" onclick="update()">登録</button>
-						<button id="dia_close" type="button" class="btn btn-default"
-							data-dismiss="modal">閉じる</button>
+						<button id="dia_close" type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 </div>
+
 <div>
-	<input id="mediums" type="hidden" value='{{ $mediums }}'>
+	<input id="mlarges" type= "hidden" value = '{{ $larges }}'>
 </div>
+<div>
+	<input id="mediums" type= "hidden" value = '{{ $mediums }}'>
+</div>
+
 <div class="container" align="center">
-	@if (Auth::user()->citycode != 00000) <input id="btn_del" type="button"
-		class="btn btn-default" value="選択行の削除" onclick="drow()"> <input
-		id="btn_ins" type="button" class="btn btn-default" value="施設登録"
-		onclick="insert()"> @endif <input id="btn_modal" type="button"
-		style="display: none" data-toggle="modal" data-target="#shosaiDialog" />
+	@if (Auth::user()->citycode != 00000)
+	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
+	<input id="btn_ins" type="button" class="btn btn-default" value="施設登録" onclick="insert()">
+	@endif
+	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog"/>
 </div>
 <script src="{{ asset('js/facility.js') }}"></script>
 
