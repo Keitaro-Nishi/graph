4a1ae69f9e8ee9e2d27e0da173d2b58dcc8c2fa8
sato taro <dsjvvknu@gmail.com>
2018-03-12 14:52:35 +0900

    a

--- app/Http/Controllers/GenreintController.php
+++ app/Http/Controllers/GenreintController.php
@@ -85,10 +85,9 @@ class GenreintController
 
 		$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents/".$g1meisho."/examples?version=2017-05-26";
 		$data = array("text" => $sword);
-
 		$jsonString = $watson->callWatson($url,$username,$password,$data);
-		error_log("●●●●●●●");
 		$json = json_decode($jsonString, true);
+		error_log("●△☓●△☓");
 
 		if($json["text"] == $sword){
 			return Response::json("OK");
