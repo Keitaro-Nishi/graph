288c5f505f4bd66c109e85b2649615d8bed9514c
sato taro <dsjvvknu@gmail.com>
2018-03-12 15:02:00 +0900

    a

--- app/Http/Controllers/GenreintController.php
+++ app/Http/Controllers/GenreintController.php
@@ -87,6 +87,7 @@ class GenreintController
 		$data = array("text" => $sword);
 		$jsonString = $watson->callWatson($url,$username,$password,$data);
 		$json = json_decode($jsonString, true);
+		error_log($json["text"]);
 
 
 		if($json["text"] == $sword){
