85c1b438d3b14e030097b8db297fd6259bc2591d
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-13 17:18:12 +0900

    s

--- public/js/facility.js
+++ public/js/facility.js
@@ -108,7 +108,7 @@ function insert() {
 
 //ジャンル選択
 function genre1change(){
-
+	var $g2value = new array();
 	$g2value = genre2value[document.getElementById('dia_genre1').value];
 	console.log($g2value);
 	document.getElementById('dia_genre2').value = $g2value;
