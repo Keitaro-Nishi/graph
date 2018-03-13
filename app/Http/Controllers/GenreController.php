9e7764797f13c923d08765d9d6c388fe30242e83
sato taro <dsjvvknu@gmail.com>
2018-03-13 15:09:31 +0900

    a

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -103,7 +103,7 @@ class GenreController
 
 			}else{
 				//DB::table('genre')->where('citycode',$cityCD)->where('gid1',$gid1)->where('gid2',$gid2)->delete();
-
+				error_log("●●●●");
 				//CVS削除
 				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities/".$gid1."/values/".urlencode($g2meisho)."?version=2017-05-26";
 				$watson->callWatson4($url,$username,$password,$cityCD);
