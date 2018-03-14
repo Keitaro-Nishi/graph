5f48ae59b9b9de43a0db46c5972379ccbe9275e2
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-14 13:50:38 +0900

    n

--- resources/views/attribute.blade.php
+++ resources/views/attribute.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.other')
 
-<title>
-	属性登録
-</title>
-
-<!-- Styles -->
-<link href="{{ asset('css/common.css') }}" rel="stylesheet">
-<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<link href="{{ asset('css/Buttons.css') }}" rel="stylesheet">
-
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
-</head>
-</html>
+@section('title')
+属性登録
+@stop
 
 @section('content')
 <div class="container">
