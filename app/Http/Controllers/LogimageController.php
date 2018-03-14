cae794d18a0eac59784acc29d9b6a76d9a51c5ae
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-14 16:05:50 +0900

    s

--- app/Http/Controllers/LogimageController.php
+++ app/Http/Controllers/LogimageController.php
@@ -12,7 +12,7 @@ class LogimageController {
 		$cityCD = Auth::user ()->citycode;
 		if ($cityCD = "00000") {
 			$logimages = Logimage::all ();
-			$results = Logimage::select( 'image' )->where('no', 1);
+			$results = Logimage::select( 'image' )->first ();
 			$img_data=pg_unescape_bytea($results);
 		} else {
 			$logimages = Logimage::where ( 'citycode', $cityCD )->get ();
