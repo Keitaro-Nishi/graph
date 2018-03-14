95e1f94cdeec831a566e617fbd309a511237da32
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-14 16:19:51 +0900

    n

--- resources/views/attribute.blade.php
+++ resources/views/attribute.blade.php
@@ -11,5 +11,5 @@
 	<input type="button" class="btn btn-primary" value="登録" onclick="update()">
 </div>
 <input id="_token" type="hidden" name="_token" value="{{ csrf_token() }}">
-<script type="text/javascript" src="js/attribute.js"></script>
+
 @endsection
