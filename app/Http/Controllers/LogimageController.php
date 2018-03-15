6f83b55bc8b5ffe3a7035b0ba4ebac19a448fc92
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-15 17:09:42 +0900

    s

--- app/Http/Controllers/LogimageController.php
+++ app/Http/Controllers/LogimageController.php
@@ -38,7 +38,7 @@ class LogimageController {
 		error_log ( '????????????????' . $id );
 		$logimages = Logimage::select ( 'image' )->where ( 'no', $id )->first ();
 		//error_log ( '40????????????????' . $logimages);
-		$response = Response::make(pg_unescape_bytea ( $logimages->image ),200);
+		$response = Response::make($logimages->image,200);
 		$response->header('Content-type','image/jpeg' );
 		//$response->header('Content-Disposition','filename=image.jpg' );
 		//header ( 'Content-type: image/jpeg' );
