48de7dfe3c99cee62604a84c47859c6ac14a0bb3
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-15 13:24:00 +0900

    n

--- app/Userinfo.php
+++ app/Userinfo.php
@@ -12,7 +12,7 @@ class Userinfo extends Model
      * @var array
      */
     protected $fillable = [
-    		'citycode','sender','userid','name','age','sex','param1','param2','param3','param4','param5',
+    		'citycode','sender','userid','name','language','age','sex','param1','param2','param3','param4','param5',
     		'param6','param7','param8','param9','param10','sposi','search','updkbn','time'
     ];
 
