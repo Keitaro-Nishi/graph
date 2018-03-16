1a3e47a75f5663182a486bbc169aed9119fc7132
sato taro <dsjvvknu@gmail.com>
2018-03-16 10:37:20 +0900

    l

--- app/Http/Controllers/WebbotController.php
+++ app/Http/Controllers/WebbotController.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class WebbotController
+{
+	public function index(Request $request)
+	{
+		return view('webbot');
+	}
+}
