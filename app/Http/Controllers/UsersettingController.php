9a1e6a80a29a1da16c3ab0b35927247aad35809a
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-16 16:53:26 +0900

    s

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class UsersettingController extends Controller {
-	public function index() {
+class UsersettingController {
+	public function index(Request $request) {
 		$userid = Auth::user ()->userid;
 		$name = Auth::user ()->name;
 		return view ( 'usersetting', [
