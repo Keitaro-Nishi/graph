b6f300bdb70674821cad5d8cdce215ad4c8c16d8
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-16 16:51:56 +0900

    s

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
