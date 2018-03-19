dd7cf98a74dcb52c24a32864db5e5d1453e91595
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-19 10:13:31 +0900

    s

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,27 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
+use App\Logindata;
 
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+class HomeController extends Controller {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware ( 'auth' );
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		$userid = Auth::user ()->userid;
+		$count = Logindata::where ( userid, $userid )->count ();
+		error_log ( $count );
+		return view ( 'home' );
+	}
 }
