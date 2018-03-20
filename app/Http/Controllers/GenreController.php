e677f479c8b38e6659049ba3b5ba820d2d4eec07
sato taro <dsjvvknu@gmail.com>
2018-03-20 18:13:49 +0900

    l

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -20,42 +20,42 @@ class GenreController
 		$genrelist= array();
 		$genrelists= array();
 
-			$genres= Genre::where('citycode', $cityCD)->orderBy('gid1', 'ASC')->orderBy('gid2', 'ASC')->get();
-			$genregid1 = Genre::select('gid1')->where('citycode', $cityCD)->get();
-			$j1values = Genre::where('citycode', $cityCD)->where('bunrui', 1)->get();
+		$genres= Genre::where('citycode', $cityCD)->orderBy('gid1', 'ASC')->orderBy('gid2', 'ASC')->get();
+		$genregid1 = Genre::select('gid1')->where('citycode', $cityCD)->get();
+		$j1values = Genre::where('citycode', $cityCD)->where('bunrui', 1)->get();
 
-			foreach ($genres as $genre) {
-				$citycode = $genre->citycode;
-				$bunrui = $genre->bunrui;
-				$daibunrui;
-				$shoubunrui;
-				$gid1 = $genre->gid1;
-				$gid2 = $genre->gid2;
-				$meisho = $genre->meisho;
+		foreach ($genres as $genre) {
+			$citycode = $genre->citycode;
+			$bunrui = $genre->bunrui;
+			$daibunrui;
+			$shoubunrui;
+			$gid1 = $genre->gid1;
+			$gid2 = $genre->gid2;
+			$meisho = $genre->meisho;
 
 
-				if($bunrui == 1){
-					$daibunrui = $meisho;
-					$shoubunrui = '-';
-				}
-
-				if($bunrui == 2){
-					$bunruidata= Genre::select('meisho')->where('citycode',$cityCD)->where('bunrui',1)->where('gid1',$gid1)->first();
-					$shoubunrui = $meisho;
-					$daibunrui= $bunruidata->meisho;
-				}
-
-				$genrelist= [
-						'citycode'=>$citycode,
-						'bunrui'=>$bunrui,
-						'daibunrui'=>$daibunrui,
-						'shoubunrui'=>$shoubunrui,
-						'gid1'=>$gid1,
-						'gid2'=>$gid2,
-				];
-
-				array_push($genrelists, $genrelist);
+			if($bunrui == 1){
+				$daibunrui = $meisho;
+				$shoubunrui = '-';
 			}
+
+			if($bunrui == 2){
+				$bunruidata= Genre::select('meisho')->where('citycode',$cityCD)->where('bunrui',1)->where('gid1',$gid1)->first();
+				$shoubunrui = $meisho;
+				$daibunrui= $bunruidata->meisho;
+			}
+
+			$genrelist= [
+					'citycode'=>$citycode,
+					'bunrui'=>$bunrui,
+					'daibunrui'=>$daibunrui,
+					'shoubunrui'=>$shoubunrui,
+					'gid1'=>$gid1,
+					'gid2'=>$gid2,
+			];
+
+			array_push($genrelists, $genrelist);
+		}
 
 		return view('genre',compact('genrelists','j1values'));
 	}
@@ -135,7 +135,6 @@ class GenreController
 		$cityCD = Auth::user()->citycode;
 		$workspace = Parameter::select('cvs_ws_id1')->where('citycode', $cityCD)->first();
 		$workspace_id = $workspace->cvs_ws_id1;
-		$meisho ="";
 
 		$input = $this->requestall;
 		$uiKbn = $input["uiKbn"];
@@ -147,106 +146,103 @@ class GenreController
 		$meishoOld = $input["meishoOld"];
 		$watson = new Watson;
 
-		if($meisho == ""){
-			if($uiKbn == 1){
-				Genre::where('citycode',$cityCD)->where('gid1',$gid1)->where('gid2',$gid2)->update(['meisho' => $meisho]);
 
-				if($gid2 == 0){
-					//大分類
-					//CVSデータ修正
-					//Intents
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents/".$gid1."?version=2017-05-26";
-					$data = array("description" => $meisho);
-					$watson->callcvsPost($cityCD,$url,$data);
-				}else{
-					//小分類
-					//CVSデータ修正
-					//ENTITIES
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities/".$gid1."/values/".urlencode($meishoOld)."?version=2017-05-26";
-					$data = array("value" => $meisho);
-					$watson->callcvsPost($cityCD,$url,$data);
+		if($uiKbn == 1){
+			Genre::where('citycode',$cityCD)->where('gid1',$gid1)->where('gid2',$gid2)->update(['meisho' => $meisho]);
 
-					//DIALOG
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/".$gid1.".".$gid2."?version=2017-05-26";
-					$data = array("conditions" => "@".$gid1.":".$meisho);
-					$watson->callcvsPost($cityCD,$url,$data);
-				}
+			if($gid2 == 0){
+				//大分類
+				//CVSデータ修正
+				//Intents
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents/".$gid1."?version=2017-05-26";
+				$data = array("description" => $meisho);
+				$watson->callcvsPost($cityCD,$url,$data);
 			}else{
-				if($bunrui == 1){
-					//大分類
+				//小分類
+				//CVSデータ修正
+				//ENTITIES
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities/".$gid1."/values/".urlencode($meishoOld)."?version=2017-05-26";
+				$data = array("value" => $meisho);
+				$watson->callcvsPost($cityCD,$url,$data);
 
-					$gid1data= Genre::select('gid1')->where('citycode', $cityCD)->orderBy('gid1', 'DESC')->first();
-
-					if(!$gid1data){
-						$gid1 =1;
-					}else{
-						$gid1 = $gid1data->gid1+1;
-					}
-					DB::table('genre')->insert(['citycode'=> $cityCD,'bunrui' =>$bunrui, 'gid1' => $gid1,'gid2' =>0,'gid3' =>0,'meisho' =>$meisho]);
-
-					//CVSデータ作成
-					//Intents
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents?version=2017-05-26";
-					$data = array("intent" => (string)$gid1,"description" => $meisho);
-					$watson->callcvsPost($cityCD,$url,$data);
-
-					//ENTITIES
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities?version=2017-05-26";
-					$data = array("entity" => (string)$gid1);
-					$watson->callcvsPost($cityCD,$url,$data);
-
-					//dialog_node
-					$previous_sibling = "";
-					$bgid1 = $gid1 - 1;
-					$nodevalue = $bgid1.".0";
-
-					//全てのLISTから１つ前のダイアログを探す
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
-					$jsonString = $watson->callcvsGet($cityCD,$url);
-					$json = json_decode($jsonString, true);
-					foreach ($json["dialog_nodes"] as $value){
-
-						if($value["output"]["text"]["values"][0] == $nodevalue){
-							$previous_sibling = $value["dialog_node"];
-							break;
-						}
-					}
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
-					$data = array("dialog_node" => $gid1.".".$gid2,"title" => "entity".$gid1,"conditions" => "@".$gid1,"previous_sibling" => "ようこそ","metadata" => array("_customization" => array("mcr" => true)));
-					$watson->callcvsPost($cityCD,$url,$data);
-					if($previous_sibling == ""){
-						$previous_sibling = $gid1.".".$gid2;
-					}
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
-					$data = array("dialog_node" => "node_".$gid1,"title" => "intent".$gid1,"conditions" => "#".$gid1,"previous_sibling" => $previous_sibling,"output" => array("text" => array("values" => array($gid1.".".$gid2))));
-					$watson->callcvsPost($cityCD,$url,$data);
-
-
-				}else{
-
-					//小分類
-					$gid2data= Genre::select('gid2')->where('citycode', $cityCD)->where('gid1',$gid1)->orderBy('gid2', 'DESC')->first();
-					$gid2 = $gid2data->gid2 + 1;
-
-					DB::table('genre')->insert(['citycode'=> $cityCD,'bunrui' => $bunrui,'gid1' => $gid1,'gid2' => $gid2,'gid3' =>0,'meisho' => $meisho]);
-
-					//CVSデータ作成
-					//ENTITIES
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities/".$gid1."/values?version=2017-05-26";
-					$data = array("value" => $meisho);
-					$watson->callcvsPost($cityCD,$url,$data);
-
-					//上記で取得したdialog_nodeをparentに設定して新規ノードを作成
-					$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
-					$data = array("dialog_node" => $gid1.".".$gid2,"type" => "response_condition","parent" =>  $gid1.".0","conditions" => "@".$gid1.":".$meisho,"output" => array("text" => array("values" => array($gid1.".".$gid2))));
-					$watson->callcvsPost($cityCD,$url,$data);
-				}
+				//DIALOG
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/".$gid1.".".$gid2."?version=2017-05-26";
+				$data = array("conditions" => "@".$gid1.":".$meisho);
+				$watson->callcvsPost($cityCD,$url,$data);
 			}
-			return \Response::json(['status' => 'OK']);
 		}else{
-			error_log("成功");
-			return \Response::json(['status' => 'NG']);
+			if($bunrui == 1){
+				//大分類
+
+				$gid1data= Genre::select('gid1')->where('citycode', $cityCD)->orderBy('gid1', 'DESC')->first();
+
+				if(!$gid1data){
+					$gid1 =1;
+				}else{
+					$gid1 = $gid1data->gid1+1;
+				}
+				DB::table('genre')->insert(['citycode'=> $cityCD,'bunrui' =>$bunrui, 'gid1' => $gid1,'gid2' =>0,'gid3' =>0,'meisho' =>$meisho]);
+
+				//CVSデータ作成
+				//Intents
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/intents?version=2017-05-26";
+				$data = array("intent" => (string)$gid1,"description" => $meisho);
+				$watson->callcvsPost($cityCD,$url,$data);
+
+				//ENTITIES
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities?version=2017-05-26";
+				$data = array("entity" => (string)$gid1);
+				$watson->callcvsPost($cityCD,$url,$data);
+
+				//dialog_node
+				$previous_sibling = "";
+				$bgid1 = $gid1 - 1;
+				$nodevalue = $bgid1.".0";
+
+				//全てのLISTから１つ前のダイアログを探す
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
+				$jsonString = $watson->callcvsGet($cityCD,$url);
+				$json = json_decode($jsonString, true);
+				foreach ($json["dialog_nodes"] as $value){
+
+					if($value["output"]["text"]["values"][0] == $nodevalue){
+						$previous_sibling = $value["dialog_node"];
+						break;
+					}
+				}
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
+				$data = array("dialog_node" => $gid1.".".$gid2,"title" => "entity".$gid1,"conditions" => "@".$gid1,"previous_sibling" => "ようこそ","metadata" => array("_customization" => array("mcr" => true)));
+				$watson->callcvsPost($cityCD,$url,$data);
+				if($previous_sibling == ""){
+					$previous_sibling = $gid1.".".$gid2;
+				}
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
+				$data = array("dialog_node" => "node_".$gid1,"title" => "intent".$gid1,"conditions" => "#".$gid1,"previous_sibling" => $previous_sibling,"output" => array("text" => array("values" => array($gid1.".".$gid2))));
+				$watson->callcvsPost($cityCD,$url,$data);
+
+
+			}else{
+
+				//小分類
+				$gid2data= Genre::select('gid2')->where('citycode', $cityCD)->where('gid1',$gid1)->orderBy('gid2', 'DESC')->first();
+				$gid2 = $gid2data->gid2 + 1;
+
+				DB::table('genre')->insert(['citycode'=> $cityCD,'bunrui' => $bunrui,'gid1' => $gid1,'gid2' => $gid2,'gid3' =>0,'meisho' => $meisho]);
+
+				//CVSデータ作成
+				//ENTITIES
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/entities/".$gid1."/values?version=2017-05-26";
+				$data = array("value" => $meisho);
+				$watson->callcvsPost($cityCD,$url,$data);
+
+				//上記で取得したdialog_nodeをparentに設定して新規ノードを作成
+				$url = "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/".$workspace_id."/dialog_nodes/?version=2017-05-26";
+				$data = array("dialog_node" => $gid1.".".$gid2,"type" => "response_condition","parent" =>  $gid1.".0","conditions" => "@".$gid1.":".$meisho,"output" => array("text" => array("values" => array($gid1.".".$gid2))));
+				$watson->callcvsPost($cityCD,$url,$data);
+			}
 		}
+		return \Response::json(['status' => 'OK']);
+
 	}
 
 
