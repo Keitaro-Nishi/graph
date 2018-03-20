4257b2e4b831b0f0c97db9270dcb62df72cda5e5
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-20 13:09:57 +0900

    n

--- app/Http/Controllers/LinepushController.php
+++ app/Http/Controllers/LinepushController.php
@@ -10,6 +10,7 @@ use App\Userinfo;
 use App\Parameter;
 use App\Code;
 use App\Pushlog;
+use App\Libs\Watson;
 
 class LinepushController
 {
@@ -89,17 +90,18 @@ class LinepushController
 	public function send()
 	{
 		//jsonを作成し、LINEに送信
+		$input = $this->requestall;
 		$cityCD = Auth::user()->citycode;
 		$line_cat = Parameter::select('line_cat')->where('citycode', $cityCD)->first();
-		$sendids= $this->makeQuerry()->get();
+		//日本語
+		$sendids= $this->makeQuerry()->where('language', '01')->get();
 		$uids = [];
 		$count = 0;
 		for ($i =0; $i < count($sendids); $i++){
 			array_push($uids,trim($sendids[$i]->userid));
 			$count = $count + 1;
 			if($count == 150){
-				$result = $this->lineSend($line_cat->line_cat,$uids);
-				error_log("★★★★★★★★★result★★★★★★★★★".$result);
+				$result = $this->lineSend($line_cat->line_cat,$uids,$input["sendmess"]);
 				if($result == "NG"){
 					return \Response::json(['status' => 'NG']);
 				}
@@ -108,12 +110,36 @@ class LinepushController
 			}
 		}
 		if(count($uids) > 0){
-			$result = $this->lineSend($line_cat->line_cat,$uids);
-			error_log("★★★★★★★★★result★★★★★★★★★".$result);
+			$result = $this->lineSend($line_cat->line_cat,$uids,$input["sendmess"]);
 			if($result == "NG"){
 				return \Response::json(['status' => 'NG']);
 			}
 		}
+		//英語
+		$sendids= $this->makeQuerry()->where('language', '02')->get();
+		$uids = [];
+		$count = 0;
+		$watson = new Watson;
+		$message = $watson->callLT($cityCD,"ja","en",$input["sendmess"]);
+		for ($i =0; $i < count($sendids); $i++){
+			array_push($uids,trim($sendids[$i]->userid));
+			$count = $count + 1;
+			if($count == 150){
+				$result = $this->lineSend($line_cat->line_cat,$uids,$message);
+				if($result == "NG"){
+					return \Response::json(['status' => 'NG']);
+				}
+				$uids = [];
+				$count = 0;
+			}
+		}
+		if(count($uids) > 0){
+			$result = $this->lineSend($line_cat->line_cat,$uids,$input["sendmess"]);
+			if($result == "NG"){
+				return \Response::json(['status' => 'NG']);
+			}
+		}
+
 
 		//ログの更新
 		$input = $this->requestall;
@@ -144,14 +170,13 @@ class LinepushController
 		return \Response::json(['status' => 'OK']);
 	}
 
-	public function lineSend($line_cat,$uids){
-		$input = $this->requestall;
+	public function lineSend($line_cat,$uids,$message){
 		$response_format_text = [
 				"to" => $uids,
 				"messages" => [
 						[
 								"type" => "text",
-								"text" => $input["sendmess"]
+								"text" => $message
 						]
 				]
 		];
@@ -169,7 +194,6 @@ class LinepushController
 		if(!curl_errno($ch)) {
 			$info = curl_getinfo($ch);
 			curl_close($ch);
-			error_log("★★★★★★★★★http_code★★★★★★★★★".$info['http_code']);
 			if($info['http_code'] == "200"){
 				return "OK";
 			}else{
