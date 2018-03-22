e960ac7fb0f10718911eebea700a864823814db9
sato taro <dsjvvknu@gmail.com>
2018-03-22 10:45:40 +0900

    l

--- app/Http/Controllers/OpinionController.php
+++ app/Http/Controllers/OpinionController.php
@@ -10,21 +10,37 @@ class OpinionController
 	{
 		$Authrole = Auth::user()->role;
 		$cityCD = Auth::user()->citycode;
+
 		if($cityCD = "00000"){
+
 			$opinions = Opinion::all();
+
 		}else{
-			$opinions= Opinion::where('citycode', $cityCD)->get();
+
+			//$opinions= Opinion::where('citycode', $cityCD)->get();
+			$opinions= Opinion::where('citycode', $cityCD)->first();
+
+			$date = date_create($opinions->time);
+			$date = date_format($date , 'Y-m-d');
+
+			error_log("☆☆☆☆☆☆");
+			error_log($date);
 		}
+
 		return view('opinion',['opinions'=>$opinions]);
 	}
+
 	public  function request(){
+
 		$this->requestall = \Request::all();
+
 		if($this->requestall["param"] == "delete"){
 			return $this->delete();
 		}else{
 			return \Response::json(['status' => 'NG']);
 		}
 	}
+
 	public function delete()
 	{
 		$input = $this->requestall;
