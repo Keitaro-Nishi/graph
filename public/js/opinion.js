c4876483cfd4aa629894b8fc3f7f99647fb26f26
sato taro <dsjvvknu@gmail.com>
2018-03-22 13:49:06 +0900

    a

--- public/js/opinion.js
+++ public/js/opinion.js
@@ -3,7 +3,7 @@ var shosai_idx = 0;
 var opinion = [];
 var dbvalue = [];
 
-//opinion= document.getElementById('opinion').value;
+opinion= document.getElementById('opinion').value;
 //dbvalue = JSON.parse(opinion);
 
 			$(function() {
@@ -87,7 +87,7 @@ var dbvalue = [];
 			function detailwin(value){
 				document.getElementById("btn_modal").click();
 				for (var i = 0; i < dbvalue.length; i++){
-					if(dbvalue[i]["id"] == value){
+					if(opinion[i]["id"] == value){
 						shosai_idx = i;
 						modal_mod(i);
 					}
@@ -104,17 +104,17 @@ var dbvalue = [];
 			}
 
 
-			/*
+
 			function modal_mod(index){
-     			document.getElementById('dia_userid').value  = dbvalue[index]["userid"];
-				document.getElementById('dia_time').value = dbvalue[index]["time"];
-				document.getElementById('dia_sadness').value  = dbvalue[index]["sadness"];
-				document.getElementById('dia_joy').value  = dbvalue[index]["joy"];
-				document.getElementById('dia_fear').value  = dbvalue[index]["fear"];
-				document.getElementById('dia_disgust').value  = dbvalue[index]["disgust"];
-				document.getElementById('dia_anger').value  = dbvalue[index]["anger"];
-				document.getElementById('dia_checked').value  = dbvalue[index]["checked"];
-				document.getElementById('dia_opinion').innerHTML  = dbvalue[index]["opinion"];
+     			document.getElementById('dia_userid').value  = opinion[index]["userid"];
+				document.getElementById('dia_time').value = opinion[index]["time"];
+				document.getElementById('dia_sadness').value  = opinion[index]["sadness"];
+				document.getElementById('dia_joy').value  = opinion[index]["joy"];
+				document.getElementById('dia_fear').value  = opinion[index]["fear"];
+				document.getElementById('dia_disgust').value  = opinion[index]["disgust"];
+				document.getElementById('dia_anger').value  = opinion[index]["anger"];
+				document.getElementById('dia_checked').value  = opinion[index]["checked"];
+				document.getElementById('dia_opinion').innerHTML  = opinion[index]["opinion"];
 				if(index == 0){
 					document.getElementById("sback").disabled = "true";
 				}else{
@@ -126,4 +126,4 @@ var dbvalue = [];
 					document.getElementById("snext").disabled = "";
 				}
 			}
-			*/
+
