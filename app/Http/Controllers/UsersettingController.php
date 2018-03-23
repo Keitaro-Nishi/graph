71b7781e00c1bc60db3d5e39e04beeab550015f2
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-03-23 15:27:45 +0900

    s

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -1,25 +1,76 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
 
 use App\User;
-use App\Http\Controllers\Controller;
+use App\Logindata;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UsersettingController {
-	
-	
-	
+	public function index(Request $request) {
+		$userid = Auth::user ()->userid;
+		$name = Auth::user ()->name;
+		$cityCD = Auth::user ()->citycode;
+		$count = Logindata::where ( 'userid', $userid )->count ();
+		return view ( 'usersetting', [
+				'userid' => $userid,
+				'name' => $name,
+				'count' => $count,
+		] );
+	}
 	public function update() {
-		$oldID = Auth::user ()->usesrid;
-		User::where ( 'userid', $oldID )->update ( [
-				'userid' => $newID ,
-				'password' => $newpassword
-		] );
-		return \Response::json ( [
-				'status' => 'OK'
-		] );
+		$userid = Auth::user ()->userid;
+		$input = \Request::all ();
+		$count = Logindata::where ( 'userid', $userid )->count ();
+
+		$rules = [
+				'name' => 'required|string|max:255',
+				'password' => 'required|string|min:6|confirmed'
+		];
+
+		$validator = Validator::make ( $input, $rules );
+
+		if ($validator->fails ()) {
+			return $validator->errors ();
+		}
+
+		$newName = $input ["name"];
+		$oldpassword = $input ["oldpassword"];
+		$password = $input ["password"];
+		$newpassword = bcrypt ( $input ["password"] );
+
+		$nowpassword = User::select ( 'password', 'name' )->where ( 'userid', $userid )->first ();
+
+		if (Hash::check ( $oldpassword, $nowpassword->password )) {
+
+			if (Hash::check ( $password, $nowpassword->password )) {
+				return \Response::json ( [
+						'status' => 'BACK'
+				] );
+			} else {
+				User::where ( 'userid', $userid )->update ( [
+						'name' => $newName,
+						'password' => $newpassword
+				] );
+				if ($count == 1) {
+					Auth::logout ();
+					return \Response::json ( [
+							'status' => 'LOGOUT'
+					] );
+				} else {
+					return \Response::json ( [
+							'status' => 'OK'
+					] );
+				}
+			}
+		} else {
+			return \Response::json ( [
+					'status' => 'NG'
+			] );
+		}
 	}
 }
