1677897beb4cae0a16a1ea428e537f22615cdecb
sato taro <dsjvvknu@gmail.com>
2018-03-23 17:42:32 +0900

    a

--- app/Http/Controllers/PushlogController.php
+++ app/Http/Controllers/PushlogController.php
@@ -55,7 +55,7 @@ class PushlogController
 					$sex = "女性";
 				}
 
-				if($agek == 999){
+				if($agek == 999 or $agem == 999){
 					$age = "すべて";
 				}else{
 					$age = $agek."歳 ～ ".$agem."歳";
@@ -113,12 +113,18 @@ class PushlogController
 					$sex = "女性";
 				}
 
+				if($agek == 999 or $agem == 999){
+					$age = "すべて";
+				}else{
+					$age = $agek."歳 ～ ".$agem."歳";
+				}
+
 				$pushloglist= [
 
 						'no'=>$no,
 						'time'=>$time,
 						'info'=>$info,
-						'age'=>$agek."歳 ～ ".$agem."歳",
+						'age'=>$age,
 						'sex'=>$sex,
 						'target'=>$target." 人",
 						'type'=>$type,
