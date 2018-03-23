4c58e4559e38b5558f7e803013faac6d42aa1ce9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-23 16:59:49 +0900

    s

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -28,7 +28,17 @@
 	<div id="app">
 		<nav class="navbar navbar-default navbar-static-top">
 			<div class="container">
-				<div class="navbar-header"></div>
+				<div class="navbar-header">
+					<!-- Collapsed Hamburger -->
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+						<span class="sr-only">Toggle Navigation</span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Branding Image -->
+					<a class="navbar-brand" href="{{ url('/home') }}">
+						<span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+					</a>
+				</div>
 			</div>
 		</nav>
 		@yield('content')
