2c4627eadfbaa7f641c384136d0fe83f32f8a2a5
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-23 13:55:33 +0900

    s

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller {
 	 *
 	 * @var string
 	 */
-
 	protected $redirectTo = '/home';
 
 	/**
@@ -42,16 +41,21 @@ class LoginController extends Controller {
 	 * $this->middleware('guest')->except('logout');
 	 * }
 	 */
+	protected function authenticated(Request $request, $user) {
+		// ログインイベントを発火させ最終ログイン日時を記録する
+		event ( new Logined () );
+	}
 
-	 protected function authenticated(Request $request, $user)
-    	{
-        // ログインイベントを発火させ最終ログイン日時を記録する
-        event(new Logined());
-   	 }
+	public function showLoginForm()
+	{
+		if (Auth::check()) {
+			return redirect ( '/home' );
+		}
+		return view('auth.login');
+	}
 
-   	 public function logout(Request $request) {
-
-   	 	event(new Logouted());
+	public function logout(Request $request) {
+		event ( new Logouted () );
 
 		$this->guard ()->logout ();
 
@@ -61,7 +65,6 @@ class LoginController extends Controller {
 
 		return redirect ( '/login' );
 	}
-
 	public function username() {
 		return 'userid';
 	}
