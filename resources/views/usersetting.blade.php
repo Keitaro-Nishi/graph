50f93e4dc8fa297d5424ebe5bce34c6bfe56d8d9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-23 13:30:35 +0900

    s

--- resources/views/usersetting.blade.php
+++ resources/views/usersetting.blade.php
@@ -48,6 +48,11 @@
 						</div>
 					</div>
 					<input id="_token" type="hidden" name="_token" value="{{ csrf_token() }}">
+					@if($count != 1)
+					<div class="text-right" >
+						<button type="button" class="btn btn-default" href="/home">ホーム</button>
+					</div>
+					@endif
 					<div class="text-right" >
 						<button type="button" class="btn btn-primary" onclick="update()">変更</button>
 					</div>
