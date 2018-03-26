446a39315898237d15b71d77f5235f83210e797c
sato taro <dsjvvknu@gmail.com>
2018-03-26 18:00:24 +0900

    l

--- public/js/pushlog.js
+++ public/js/pushlog.js
@@ -18,7 +18,7 @@ $(function() {
 		columnSelection : false,
 		formatters: {
 			"details": function($column, $row) {
-				return "<input type='button' class='btn btn-default' value='対象情報' onclick='detailwin("  + $row.no + " " + $row.param1 + ")'> ";
+				return "<input type='button' class='btn btn-default' value='対象情報' onclick='detailwin("  + $row.no + $row.param1 + ")'> ";
 			}
 		}
 	}).on("selected.rs.jquery.bootgrid", function(e, rows) {
