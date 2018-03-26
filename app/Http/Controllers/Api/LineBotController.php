65b0c06ffc965e4a64bf1ffd635d13e605766baa
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-03-26 15:36:04 +0900

    n

--- app/Http/Controllers/Api/LineBotController.php
+++ app/Http/Controllers/Api/LineBotController.php
@@ -4,47 +4,141 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Parameter;
 use App\Message;
+use App\Code;
 class LineBotController
 {
+
+	private $citycode;
+	private $jsonRequest;
+
 	public function callback(Request $request,$citycode)
 	{
 
 		error_log("★★★★★★★★★★★★citycode★★★★★★★★★★★★★".$citycode);
 		error_log("★★★★★★★★★★★★req★★★★★★★★★★★★★".$request->getContent());
+		error_log("★★★★★★★★★★★★SERVER★★★★★★★★★★★★★".$_SERVER["HTTP_HOST"]);
 
-		$jsonObj = json_decode($request->getContent());
+		$this->citycode = $citycode;
+		$this->jsonRequest = json_decode($request->getContent());
 
-		$eventType = $jsonObj->{"events"}[0]->{"type"};
-		//ReplyToken取得
-		$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+		$eventType = $this->jsonRequest->{"events"}[0]->{"type"};
 
-		//友達追加時の処理
-		if($eventType == "follow"){
-			$mess = Message::select('message')->where('citycode', $citycode)->where('id', 1)->first();
-			$response_format_text = [
-					"type" => "text",
-					"text" => $mess->message
-			];
-			$this->linesend($citycode,$replyToken,$response_format_text);
-			return;
+		switch ($eventType) {
+			//友達追加時の処理
+			case "follow":
+				$this->followEvent();
+				break;
+			case "message":
+				$this->messageEvent();
+				break;
+			default:
+				error_log("★★★★★★★★★★★★eventType★★★★★★★★★★★★★".$eventType);
+		}
+	}
+
+	public function followEvent (){
+		$mess = Message::select('message')->where('citycode', $citycode)->where('id', 1)->first();
+		$response_format_text = [
+				"type" => "text",
+				"text" => $mess->message
+		];
+		$this->linesend($response_format_text);
+	}
+
+	public function messageEvent(){
+		$type = $this->jsonRequest->{"events"}[0]->{"message"}->{"type"};
+
+		switch ($type) {
+			case "text":
+				$this->textMessage();
+				break;
+			case "image":
+				$this->imageMessage();
+				break;
+			case "location":
+				$this->locationMessage();
+				break;
+			default:
+				error_log("★★★★★★★★★★★★type★★★★★★★★★★★★★".$type);
 		}
 
-		error_log("★★★★★★★★★★★★type★★★★★★★★★★★★★".$type);
-		$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
-		//メッセージ取得
-		$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
 		//ユーザーID取得
-		$userID = $jsonObj->{"events"}[0]->{"source"}->{"userId"};
-
-		error_log("★★★★★★★★★★★★replyToken★★★★★★★★★★★★★".$replyToken);
-		error_log("★★★★★★★★★★★★eventType★★★★★★★★★★★★★".$eventType);
-
+		$userID = $this->jsonRequest->{"events"}[0]->{"source"}->{"userId"};
 
 
 	}
 
-	public function linesend ($citycode,$replyToken,$response_format_text){
-		$line_cat = Parameter::select('line_cat')->where('citycode', $citycode)->first();
+	public function textMessage(){
+		//メッセージ取得
+		$text = $this->jsonRequest->{"events"}[0]->{"message"}->{"text"};
+
+		$functions = Code::where('citycode', '00000')->where('code1', (int)13)->orderBy('code2', 'ASC')->get();
+		$usefunction = Parameter::select('usefunction')->where('citycode', $this->citycode)->first();
+		$unknownMess = Message::select('message')->where('citycode', $citycode)->where('id', 2)->first();
+		for ($i =0; $i < count($functions); $i++){
+			if($functions[$i]->meisho == $text){
+				switch ($functions[$i]->code2) {
+					//属性登録
+					case 1:
+						if(substr($usefunction->usefunction,$i,0) == 1){
+							$response_format_text = [
+									"type" => "text",
+									"text" => "属性登録"
+							];
+							$this->linesend($response_format_text);
+						}else{
+							$response_format_text = [
+									"type" => "text",
+									"text" => $unknownMess->message
+							];
+							$this->linesend($response_format_text);
+						}
+						return;
+					//検診相談
+					case 2:
+						$this->imageMessage();
+						return;
+					//問い合わせ
+					case 3:
+						$this->locationMessage();
+						return;
+					//ごみの分別
+					case 4:
+						$this->locationMessage();
+						return;
+					//周辺施設検索
+					case 5:
+						$this->locationMessage();
+						return;
+					//市政へのご意見
+					case 6:
+						$this->locationMessage();
+						return;
+					//周辺施設検索
+					case 5:
+						$this->locationMessage();
+						return;
+					default:
+						break;
+				}
+			}
+		}
+	}
+
+	public function imageMessage(){
+
+	}
+
+	public function locationMessage(){
+
+	}
+
+	public function linesend ($response_format_text){
+
+		//ReplyToken取得
+		$replyToken = $this->jsonRequest->{"events"}[0]->{"replyToken"};
+
+		$line_cat = Parameter::select('line_cat')->where('citycode', $this->citycode)->first();
 
 		$post_data = [
 				"replyToken" => $replyToken,
