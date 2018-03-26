0dc4ebe77a30125942c37149f621a38c9f896039
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-26 15:11:59 +0900

    s

--- public/js/opinion.js
+++ public/js/opinion.js
@@ -116,7 +116,8 @@ function modal_mod(index){
 	document.getElementById('dia_checked').value  = dbvalue[index]["checked"];
 	document.getElementById('dia_opinion').innerHTML  = dbvalue[index]["opinion"];
 
-	var myChart = new Chart(ctx, {
+	var myChart = new Chart(ctx);
+	$('myChart').on('shown.bs.modal', function (event) {
 		type: 'bar',
 		data: {
 			labels: ['悲しみ', '喜び', '恐れ', '嫌悪', '怒り'],
