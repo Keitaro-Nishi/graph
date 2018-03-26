7bb8cc5dc2571c86dfa78f369296b777cdda63c2
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-26 10:16:20 +0900

    s

--- resources/views/opinion.blade.php
+++ resources/views/opinion.blade.php
@@ -79,34 +79,32 @@
 							<textarea class="form-control" id="dia_opinion" rows='5' readonly></textarea>
 						</div>
 					</div>
-					<div  id="disp">
-						<div class="form-group">
-							<label class="col-sm-2 control-label" for="dia_sadness">悲しみ</label>
-							<div class="col-sm-2">
-								<input type="text" class="form-control" id="dia_sadness" readonly>
-							</div>
-							<label class="col-sm-2 control-label" for="dia_joy">喜び</label>
-							<div class="col-sm-2">
-								<input type="text" class="form-control" id="dia_joy" readonly>
-							</div>
-							<label class="col-sm-2 control-label" for="dia_fear">恐れ</label>
-							<div class="col-sm-2">
-								<input type="text" class="form-control" id="dia_fear" readonly>
-							</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_sadness">悲しみ</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_sadness" readonly>
 						</div>
-						<div class="form-group">
-							<label class="col-sm-2 control-label" for="dia_disgust">嫌悪</label>
-							<div class="col-sm-2">
-								<input type="text" class="form-control" id="dia_disgust" readonly>
-							</div>
-							<label class="col-sm-2 control-label" for="dia_anger">怒り</label>
-							<div class="col-sm-2">
-								<input type="text" class="form-control" id="dia_anger" readonly>
-							</div>
-							<label class="col-sm-2 control-label" for="dia_checked">チェック</label>
-							<div class="col-sm-2">
-								<input type="text" class="form-control" id="dia_checked" readonly>
-							</div>
+						<label class="col-sm-2 control-label" for="dia_joy">喜び</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_joy" readonly>
+						</div>
+						<label class="col-sm-2 control-label" for="dia_fear">恐れ</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_fear" readonly>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_disgust">嫌悪</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_disgust" readonly>
+						</div>
+						<label class="col-sm-2 control-label" for="dia_anger">怒り</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_anger" readonly>
+						</div>
+						<label class="col-sm-2 control-label" for="dia_checked">チェック</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="dia_checked" readonly>
 						</div>
 					</div>
 				</form>
