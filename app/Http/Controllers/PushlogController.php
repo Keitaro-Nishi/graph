089569c18d009ea2b2f69f4c9607951751155752
sato taro <dsjvvknu@gmail.com>
2018-03-26 11:11:48 +0900

    a

--- app/Http/Controllers/PushlogController.php
+++ app/Http/Controllers/PushlogController.php
@@ -150,8 +150,7 @@ class PushlogController
 		for ($i = 1; $i <= 10; $i++) {
 			$count = Code::where('citycode', $cityCD)->where('code1', $i)->where('code2', '>', 0)->count();
 			if($count > 0){
-				$records = Code::select('code1','meisho')->where('citycode', $cityCD)->where('code1', $i)->orderBy('code2', 'ASC')->get();
-				//$records = Code::select('code1','meisho')->where('citycode', $cityCD)->where('code1', $i)->where('code2', 0)->orderBy('code2', 'ASC')->get();
+				$records = Code::select('code1','code2','meisho')->where('citycode', $cityCD)->where('code1', $i)->orderBy('code2', 'ASC')->get();
 				array_push($codes, json_decode($records,true));
 			}
 		}
