e4d097ff534d189772c4e8889dd1e63f7333f12a
sato taro <dsjvvknu@gmail.com>
2018-03-26 11:02:39 +0900

    a

--- app/Http/Controllers/PushlogController.php
+++ app/Http/Controllers/PushlogController.php
@@ -150,14 +150,11 @@ class PushlogController
 		for ($i = 1; $i <= 10; $i++) {
 			$count = Code::where('citycode', $cityCD)->where('code1', $i)->where('code2', '>', 0)->count();
 			if($count > 0){
-				//$records = Code::select('code1','code2','meisho')->where('citycode', $cityCD)->where('code1', $i)->orderBy('code2', 'ASC')->get();
-				$records = Code::select('code1','meisho')->where('citycode', $cityCD)->where('code1', $i)->where('code2', 0)->orderBy('code2', 'ASC')->get();
+				$records = Code::select('code1','code2','meisho')->where('citycode', $cityCD)->where('code1', $i)->orderBy('code2', 'ASC')->get();
+				//$records = Code::select('code1','meisho')->where('citycode', $cityCD)->where('code1', $i)->where('code2', 0)->orderBy('code2', 'ASC')->get();
 				array_push($codes, json_decode($records,true));
 			}
 		}
-
-		error_log("★★★★★★");
-		error_log(print_r($codes,true));
 
 		return view('pushlog',compact('pushlogs','pushlogvalue','codes'));
 	}
