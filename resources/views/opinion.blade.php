03d04959b787b64c6206bbf2a4d03053138f0234
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-26 15:35:07 +0900

    s

--- resources/views/opinion.blade.php
+++ resources/views/opinion.blade.php
@@ -117,7 +117,7 @@
 					<canvas id="myChart"></canvas>
 				</div>
 			</div>
-			<br><br><br><br><br>
+			<br><br><br><br><br><br>
 			<div class="modal-footer">
 				<button id="sback" type="button" class="btn btn-default" onclick="shosai_back()">＜＜前へ</button>
 				<button id="snext" type="button" class="btn btn-default" onclick="shosai_next()">次へ＞＞</button>
