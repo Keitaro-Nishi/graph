ccef2061cffd31b412dfc8b18ac92768d856bbe7
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-26 19:16:33 +0900

    s

--- app/Http/Controllers/BotlogController.php
+++ app/Http/Controllers/BotlogController.php
@@ -1,71 +1,73 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Botlog;
-class BotlogController
-{
-	public function index(Request $request)
-	{
-		$cityCD = Auth::user()->citycode;
-		$botloglist= array();
-		$botlogs= array();
-		if($cityCD == "00000"){
-			$botlogdatas = Botlog::all();
-			foreach($botlogdatas as $botlogdata){
+
+class BotlogController {
+	public function index(Request $request) {
+		$cityCD = Auth::user ()->citycode;
+		$botloglist = array ();
+		$botlogs = array ();
+		if ($cityCD == "00000") {
+			$botlogdatas = Botlog::all ();
+			foreach ( $botlogdatas as $botlogdata ) {
 				$citycode = $botlogdata->citycode;
 				$no = $botlogdata->no;
-				$timedata = date_create($botlogdata->time);
-				$time = date_format($timedata , 'Y/m/d H:i:s');
+				$timedata = date_create ( $botlogdata->time );
+				$time = date_format ( $timedata, 'Y/m/d H:i:s' );
 				$sender = $botlogdata->sender;
 				$type = $botlogdata->type;
 				$userid = $botlogdata->userid;
 				$contents = $botlogdata->contents;
 				$return = $botlogdata->return;
-				$botloglist= [
-						'citycode'=>$citycode,
-						'no'=>$no,
-						'time'=>$time,
-						'sender'=>$sender,
-						'type'=>$type,
-						'userid'=>$userid,
-						'contents'=>$contents,
-						'return'=>$return,
+				$botloglist = [
+						'citycode' => $citycode,
+						'no' => $no,
+						'time' => $time,
+						'sender' => $sender,
+						'type' => $type,
+						'userid' => $userid,
+						'contents' => $contents,
+						'return' => $return
 				];
-				array_push($botlogs, $botloglist);
+				array_push ( $botlogs, $botloglist );
 			}
-		}else{
-			$botlogdatas= Botlog::where('citycode', $cityCD)->get();
-			foreach($botlogdatas as $botlogdata){
+		} else {
+			$botlogdatas = Botlog::where ( 'citycode', $cityCD )->get ();
+			foreach ( $botlogdatas as $botlogdata ) {
 				$citycode = $botlogdata->citycode;
 				$no = $botlogdata->no;
-				$timedata = date_create($botlogdata->time);
-				$time = date_format($timedata , 'Y/m/d H:i:s');
+				$timedata = date_create ( $botlogdata->time );
+				$time = date_format ( $timedata, 'Y/m/d H:i:s' );
 				$sender = $botlogdata->sender;
 				$type = $botlogdata->type;
 				$userid = $botlogdata->userid;
 				$contents = $botlogdata->contents;
 				$return = $botlogdata->return;
-				$botloglist= [
-						'citycode'=>$citycode,
-						'no'=>$no,
-						'time'=>$time,
-						'sender'=>$sender,
-						'type'=>$type,
-						'userid'=>$userid,
-						'contents'=>$contents,
-						'return'=>$return,
+				$botloglist = [
+						'citycode' => $citycode,
+						'no' => $no,
+						'time' => $time,
+						'sender' => $sender,
+						'type' => $type,
+						'userid' => $userid,
+						'contents' => $contents,
+						'return' => $return
 				];
-				array_push($botlogs, $botloglist);
+				array_push ( $botlogs, $botloglist );
 			}
 		}
+		$today = Carbon::now ();
+		$result = Botlog::where ( 'time', $today )->groupBy ( 'time' )->orderBy ( 'time', 'ASC' )->count ();
+		error_log ( "?????????????" . $today . "???????????????" );
 
-		//$result = Botlog::whrer('time')-> groupBy('time')-> orderBy ('time', 'ASC')-> count();
-
-		$botlogvalue= json_encode($botlogs);
-		return view('botlog',compact('botlogs','botlogvalue'));
+		$botlogvalue = json_encode ( $botlogs );
+		return view ( 'botlog', compact ( 'botlogs', 'botlogvalue' ) );
 	}
 	public function request() {
 		$this->requestall = \Request::all ();
@@ -79,9 +81,11 @@ class BotlogController
 			] );
 		}
 	}
-	public function delete(){
+	public function delete() {
 		$input = $this->requestall;
-		Botlog::destroy($input["nos"]);
-		return \Response::json(['status' => 'OK']);
+		Botlog::destroy ( $input ["nos"] );
+		return \Response::json ( [
+				'status' => 'OK'
+		] );
 	}
 }
