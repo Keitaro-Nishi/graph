f8f6f80aef9706a78ff9a6c2a4f9601270a504b1
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-26 13:52:48 +0900

    s

--- resources/views/opinion.blade.php
+++ resources/views/opinion.blade.php
@@ -112,8 +112,8 @@
 				</form>
 			</div>
 			<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-			<div class="chart-container" style="position: relative; height:20vh; width:40vw">
-				<canvas id="myChart" width="600" height="250"></canvas>
+			<div class="chart-container" style="position: relative; height:40vh; width:80vw">
+				<canvas id="myChart"></canvas>
 			</div>
 			<div class="modal-footer">
 				<button id="sback" type="button" class="btn btn-default" onclick="shosai_back()">＜＜前へ</button>
