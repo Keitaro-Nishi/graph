b20dde3b2966d7906d07ec0581ed1fc3a1881962
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-26 13:20:03 +0900

    s

--- resources/views/opinion.blade.php
+++ resources/views/opinion.blade.php
@@ -112,7 +112,7 @@
 				</form>
 			</div>
 			<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-			<div align="center" style="position: relative; width: 800px;">
+			<div align="center">
 				<canvas id="myChart" width="600" height="250"></canvas>
 			</div>
 			<div class="modal-footer">
