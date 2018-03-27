fb728fd3b9f7e557de1e92ba2da87de68efb7127
sato taro <dsjvvknu@gmail.com>
2018-03-27 14:35:31 +0900

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,28 @@
 	<br>
 	<br>
 	<br>
-	<a href="/botlog" class="homebutton">
-	  <div>
-	    <span>ログ参照</span>
-	  </div>
-	  <p>クリック </p>
-	</a>
-	<a href="/logimage" class="homebutton">
-	  <div>
-	    <span>画像ログ参照</span>
-	  </div>
-	  <p>クリック</p>
-	</a>
-	<br>
-	<br>
-	<br>
-	<a href="/facility" class="homebutton">
-	  <div>
-	    <span>施設情報</span>
-	  </div>
-	  <p>クリック</p>
-	</a>
-
+		<button onclick="location.href='/botlog'" class="homebutton" type="submit">ログ参照</button>
+		<button onclick="location.href='/logimage'" class="homebutton" type="submit">画像ログ参照</button>
+		<button onclick="location.href='/facility'" class="homebutton" type="submit">施設情報</button>
+		<button onclick="location.href='/genre'" class="homebutton" type="submit">施設ジャンル</button>
+		<br>
+		<br>
+		<br>
+		<button onclick="location.href='/opinion'" class="homebutton" type="submit">ご意見ログ</button>
+		<button onclick="location.href='/logview'" class="homebutton" type="submit">属性情報</button>
+		<button onclick="location.href='/linepush'" class="homebutton" type="submit">LINEプッシュ通知</button>
+		<button onclick="location.href='/pushlog'" class="homebutton" type="submit">LINEプッシュログ</button>
+		<button onclick="location.href='/messagemanager'" class="homebutton" type="submit">メッセージ管理</button>
+		<br>
+		<br>
+		<br>
+		<button onclick="location.href='/webbot'" class="homebutton" type="submit">チャットボット WEB </button>
+		<button onclick="location.href='/logview'" class="homebutton" type="submit">セッション情報</button>
+		<button onclick="location.href='/codemanage'" class="homebutton" type="submit">コード管理</button>
+		<button onclick="location.href='/parameter'" class="homebutton" type="submit">市町村パラメタ</button>
+		<br>
+		<br>
+		<br>
 	</div>
 
 @endsection
