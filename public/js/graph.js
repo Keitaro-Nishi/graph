57f5af04f83361aa63ad456584160261fb5708e6
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-27 13:55:15 +0900

    s

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,10 +1,29 @@
 $(function() {
-	document.getElementById('graph1').style.display="none";
+	//document.getElementById('graph1').style.display="none";
 	$('#date_from').datepicker({
 	    dateFormat: 'yy/mm/dd',//年月日の並びを変更
 	});
 	$('#date_to').datepicker({
 	    dateFormat: 'yy/mm/dd',//年月日の並びを変更
+	});
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+	  type: 'doughnut',
+	  data: {
+	    labels: ["M", "T", "W", "T", "F", "S", "S"],
+	    datasets: [{
+	      backgroundColor: [
+	        "#2ecc71",
+	        "#3498db",
+	        "#95a5a6",
+	        "#9b59b6",
+	        "#f1c40f",
+	        "#e74c3c",
+	        "#34495e"
+	      ],
+	      data: [12, 19, 3, 17, 28, 24, 7]
+	    }]
+	  }
 	});
 });
 
