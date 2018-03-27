02cf62ba2511bf9e3e07c4d0a99db2f2da796739
sato taro <dsjvvknu@gmail.com>
2018-03-27 14:58:28 +0900

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,13 @@
 	<br>
 		<button onclick="location.href='/botlog'" class="homebutton" type="submit">ログ参照</button>
 		<button onclick="location.href='/logimage'" class="homebutton" type="submit">画像ログ参照</button>
-		<button onclick="location.href='/webbot'" class="homebutton" type="submit">チャットボット WEB </button>
-		<br>
-		<br>
-		<br>
 		<button onclick="location.href='/facility'" class="homebutton" type="submit">施設情報</button>
 		<button onclick="location.href='/genre'" class="homebutton" type="submit">施設ジャンル</button>
+		<br>
+		<br>
+		<br>
+		<button onclick="location.href='/webbot'" class="homebutton" type="submit">チャットボット WEB </button>
 		<button onclick="location.href='/opinion'" class="homebutton" type="submit">ご意見ログ</button>
-		<br>
-		<br>
-		<br>
 		<button onclick="location.href='/linepush'" class="homebutton" type="submit">LINEプッシュ通知</button>
 		<button onclick="location.href='/pushlog'" class="homebutton" type="submit">LINEプッシュログ</button>
 		<button onclick="location.href='/messagemanager'" class="homebutton" type="submit">メッセージ管理</button>
