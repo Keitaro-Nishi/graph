64c651d69f39b71b1f267b1c0db2dcafebb2ce99
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-27 14:49:40 +0900

    s

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,30 +1,69 @@
 $(function() {
 	$('#date_from').datepicker({
-	    dateFormat: 'yy/mm/dd',//年月日の並びを変更
+		dateFormat: 'yy/mm/dd',//年月日の並びを変更
 	});
 	$('#date_to').datepicker({
-	    dateFormat: 'yy/mm/dd',//年月日の並びを変更
+		dateFormat: 'yy/mm/dd',//年月日の並びを変更
+	});
+	var ctx = document.getElementById("myChart1").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'doughnut',
+		data: {
+			labels: ["M", "T", "W", "T", "F", "S", "S"],
+			datasets: [{
+				backgroundColor: [
+					"#2ecc71",
+					"#3498db",
+					"#95a5a6",
+					"#9b59b6",
+					"#f1c40f",
+					"#e74c3c",
+					"#34495e"
+				],
+				data: [12, 19, 3, 17, 28, 24, 7]
+			}]
+		}
+	});
+	var ctx = document.getElementById("myChart2").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'doughnut',
+		data: {
+			labels: ["M", "T", "W", "T", "F", "S", "S"],
+			datasets: [{
+				backgroundColor: [
+					"#2ecc71",
+					"#3498db",
+					"#95a5a6",
+					"#9b59b6",
+					"#f1c40f",
+					"#e74c3c",
+					"#34495e"
+				],
+				data: [12, 19, 3, 17, 28, 24, 7]
+			}]
+		}
 	});
 });
-
+/*
 function open(){
 	var ctx = document.getElementById("myChart").getContext('2d');
 	var myChart = new Chart(ctx, {
-	  type: 'doughnut',
-	  data: {
-	    labels: ["M", "T", "W", "T", "F", "S", "S"],
-	    datasets: [{
-	      backgroundColor: [
-	        "#2ecc71",
-	        "#3498db",
-	        "#95a5a6",
-	        "#9b59b6",
-	        "#f1c40f",
-	        "#e74c3c",
-	        "#34495e"
-	      ],
-	      data: [12, 19, 3, 17, 28, 24, 7]
-	    }]
-	  }
+		type: 'doughnut',
+		data: {
+			labels: ["M", "T", "W", "T", "F", "S", "S"],
+			datasets: [{
+				backgroundColor: [
+					"#2ecc71",
+					"#3498db",
+					"#95a5a6",
+					"#9b59b6",
+					"#f1c40f",
+					"#e74c3c",
+					"#34495e"
+				],
+				data: [12, 19, 3, 17, 28, 24, 7]
+			}]
+		}
 	});
 }
+*/
--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -14,10 +14,15 @@
 					<input type="text" name="date_from" id="date_from" readonly="readonly">から
 					<input type="text" name="date_to" id="date_to" />まで
 				</form>
-				<button id="open" type="button" class="btn btn-default" onclick="open()">表示</button>
-				<div class="container">
+				<button id="open" type="button" class="btn btn-default">表示</button>
+				<div class="container" style="position:relative; height:120px; width:120px">
 					<div>
-						<canvas id="myChart"></canvas>
+						<canvas id="myChart1"></canvas>
+					</div>
+				</div>
+				<div class="container" style="position:relative; height:120px; width:120px">
+					<div>
+						<canvas id="myChart2"></canvas>
 					</div>
 				</div>
 			</form>
