275bd7269a72ef5c90c17a68e4456cf1a26569ff
sato taro <dsjvvknu@gmail.com>
2018-03-27 14:20:22 +0900

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
+	<div align="center">
+	<br>
+	<br>
+	<br>
+	<br>
 	<a href="/botlog" class="homebutton">
 	  <div>
 	    <span>ログ参照</span>
 	  </div>
 	  <p>CLICK !</p>
 	</a>
+	</div>
 
 @endsection
