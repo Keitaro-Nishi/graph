734a9c283d9c488d12d1a02e65ec2f73f271d1b9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-27 15:47:11 +0900

    s

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -16,12 +16,12 @@
 				</form>
 				<button id="open" type="button" class="btn btn-default">表示</button>
 				<div class="container" style="position:relative; height:400px; width:400px">
-					<div>
+					<div align="left">
 						<canvas id="myChart1"></canvas>
 					</div>
 				</div>
 				<div class="container" style="position:relative; height:400px; width:400px">
-					<div>
+					<div align="right">
 						<canvas id="myChart2"></canvas>
 					</div>
 				</div>
