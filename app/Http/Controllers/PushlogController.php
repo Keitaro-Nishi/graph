353932b6f74f3206e80edd3a46beea6ca7fec86c
sato taro <dsjvvknu@gmail.com>
2018-03-27 09:26:08 +0900

    a

--- app/Http/Controllers/PushlogController.php
+++ app/Http/Controllers/PushlogController.php
@@ -30,61 +30,82 @@ class PushlogController
 
 		foreach($pushlogdatas as $pushlogdata){
 
-			    $no = $pushlogdata->no;
-				$timedata = date_create($pushlogdata->time);
-				$time = date_format($timedata , 'Y-m-d H:i:s');
-				$infodata = $pushlogdata->info;
-				$agek = $pushlogdata->agek;
-				$agem = $pushlogdata->agem;
-				$sexdata = $pushlogdata->sex;
-				$target = $pushlogdata->target;
-				$typedata = $pushlogdata->type;
-				$contents = $pushlogdata->contents;
-				$senderdata = $pushlogdata->sender;
-				$senders = User::select('name')->where('userid',$senderdata)->first();
-				$sender = $senders->name;
+			$no = $pushlogdata->no;
+			$timedata = date_create($pushlogdata->time);
+			$time = date_format($timedata , 'Y-m-d H:i:s');
+			$infodata = $pushlogdata->info;
+			$agek = $pushlogdata->agek;
+			$agem = $pushlogdata->agem;
+			$sexdata = $pushlogdata->sex;
+			$param1 = $pushlogdata->param1;
+			$param2 = $pushlogdata->param2;
+			$param3 = $pushlogdata->param3;
+			$param4 = $pushlogdata->param4;
+			$param5 = $pushlogdata->param5;
+			$param6 = $pushlogdata->param6;
+			$param7 = $pushlogdata->param7;
+			$param8 = $pushlogdata->param8;
+			$param9 = $pushlogdata->param9;
+			$param10 = $pushlogdata->param10;
 
-				$typevalue = Code::select('meisho')->where('citycode',"00000")->where('code1',15)->where('code2',$typedata)->first();
-				$type = $typevalue->meisho;
+			$target = $pushlogdata->target;
+			$typedata = $pushlogdata->type;
+			$contents = $pushlogdata->contents;
+			$senderdata = $pushlogdata->sender;
+			$senders = User::select('name')->where('citycode',$cityCD)->where('userid',$senderdata)->first();
+			$sender = $senders->name;
 
-				if($infodata == 0){
-					$info = "すべて";
-				}elseif($infodata == 1){
-					$info = "登録あり";
-				}elseif($infodata == 2){
-					$info = "登録なし";
-				}
+			$typevalue = Code::select('meisho')->where('citycode',"00000")->where('code1',15)->where('code2',$typedata)->first();
+			$type = $typevalue->meisho;
 
-				if($sexdata == 0){
-					$sex = "すべて";
-				}elseif($sexdata== 1){
-					$sex = "男性";
-				}elseif($sexdata== 2){
-					$sex = "女性";
-				}
+			if($infodata == 0){
+				$info = "すべて";
+			}elseif($infodata == 1){
+				$info = "登録あり";
+			}elseif($infodata == 2){
+				$info = "登録なし";
+			}
 
-				if($agek == 999 or $agem == 999){
-					$age = "すべて";
-				}elseif($agek == $agem){
-					$age = $agek."歳";
-				}else{
-					$age = $agek."歳 ～ ".$agem."歳";
-				}
+			if($sexdata == 0){
+				$sex = "すべて";
+			}elseif($sexdata== 1){
+				$sex = "男性";
+			}elseif($sexdata== 2){
+				$sex = "女性";
+			}
 
-				$pushloglist= [
+			if($agek == 999 or $agem == 999){
+				$age = "すべて";
+			}elseif($agek == $agem){
+				$age = $agek."歳";
+			}else{
+				$age = $agek."歳 ～ ".$agem."歳";
+			}
 
-						'no'=>$no,
-						'time'=>$time,
-						'info'=>$info,
-						'age'=>$age,
-						'sex'=>$sex,
-						'target'=>$target." 人",
-						'type'=>$type,
-						'contents'=>$contents,
-						'sender'=>$sender,
-				];
+			$pushloglist= [
 
-				array_push($pushlogs, $pushloglist);
+					'no'=>$no,
+					'time'=>$time,
+					'info'=>$info,
+					'age'=>$age,
+					'sex'=>$sex,
+					'param1'=>$param1,
+					'param2'=>$param2,
+					'param3'=>$param3,
+					'param4'=>$param4,
+					'param5'=>$param5,
+					'param6'=>$param6,
+					'param7'=>$param7,
+					'param8'=>$param8,
+					'param9'=>$param9,
+					'param10'=>$param10,
+					'target'=>$target." 人",
+					'type'=>$type,
+					'contents'=>$contents,
+					'sender'=>$sender,
+			];
+
+			array_push($pushlogs, $pushloglist);
 		}
 
 
