01a8dca9c06cfd97295928533501d12517c943f9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-27 18:04:32 +0900

    s

--- public/js/graph.js
+++ public/js/graph.js
@@ -5,8 +5,6 @@ function init() {
 	$('#date_to').datepicker({
 		dateFormat: 'yy/mm/dd',//年月日の並びを変更
 	});
-	date_from = document.getElementById('date_from').value;
-	console.log(date_from);
 	var ctx = document.getElementById("myChart1").getContext('2d');
 	var myChart = new Chart(ctx, {
 		type: 'doughnut',
@@ -48,26 +46,7 @@ function init() {
 		}
 	});
 }
-/*
 function open(){
-	var ctx = document.getElementById("myChart").getContext('2d');
-	var myChart = new Chart(ctx, {
-		type: 'doughnut',
-		data: {
-			labels: ["M", "T", "W", "T", "F", "S", "S"],
-			datasets: [{
-				backgroundColor: [
-					"#2ecc71",
-					"#3498db",
-					"#95a5a6",
-					"#9b59b6",
-					"#f1c40f",
-					"#e74c3c",
-					"#34495e"
-				],
-				data: [12, 19, 3, 17, 28, 24, 7]
-			}]
-		}
-	});
+	date_from = document.getElementById('date_from').value;
+	console.log(date_from);
 }
- */
--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -14,7 +14,7 @@
 					<input type="text" name="date_from" id="date_from" readonly="readonly">から
 					<input type="text" name="date_to" id="date_to" />まで
 				</form>
-				<button id="open" type="button" class="btn btn-default">表示</button>
+				<button id="open" type="button" class="btn btn-default" onclick="open()">表示</button>
 				<div class="container" style="position:relative; height:400px; width:400px">
 					<div class="chart">
 						<canvas id="myChart1"></canvas>
