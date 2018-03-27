41bfee4fabd4797bfd73071649ff893f98ce4d82
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-27 13:33:19 +0900

    s

--- public/js/graph.js
+++ public/js/graph.js
@@ -2,7 +2,6 @@ $(function() {
 	$('#date_from').datepicker({
 	    dateFormat: 'yy/mm/dd',//年月日の並びを変更
 	});
-	console.log(#date_from);
 	$('#date_to').datepicker({
 	    dateFormat: 'yy/mm/dd',//年月日の並びを変更
 	});
