21279471d1e20b1604dd78ece866a6293fdf4a01
sato taro <dsjvvknu@gmail.com>
2018-03-27 14:27:33 +0900

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,24 @@
 	  <div>
 	    <span>ログ参照</span>
 	  </div>
-	  <p>CLICK !</p>
+	  <p>クリック </p>
 	</a>
+	<a href="/logimage" class="homebutton">
+	  <div>
+	    <span>画像ログ参照</span>
+	  </div>
+	  <p>クリック</p>
+	</a>
+	<br>
+	<br>
+	<br>
+	<a href="/facility" class="homebutton">
+	  <div>
+	    <span>施設情報</span>
+	  </div>
+	  <p>クリック</p>
+	</a>
+
 	</div>
 
 @endsection
