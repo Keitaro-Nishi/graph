e79bd437dd3c61b2c437112ff6e1a959813e423d
sato taro <dsjvvknu@gmail.com>
2018-03-27 15:27:06 +0900

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,13 +78,11 @@
 								<li><a href="{{ route('facility') }}">施設情報</a></li>
 								<li><a href="{{ route('genre') }}">施設ジャンル</a></li>
 								<li><a href="{{ route('opinion') }}">ご意見ログ</a></li>
-								<li><a href="{{ route('register') }}">属性情報</a></li>
 								<li><a href="{{ route('linepush') }}">LINEプッシュ通知</a></li>
 								<li><a href="{{ route('pushlog') }}">LINEプッシュログ</a></li>
 								<li><a href="{{ route('messagemanage') }}">メッセージ管理</a></li>
 								<li><a href="{{ route('webbot') }}">チャットボット WEB</a></li>
 								@if (Auth::user()->role == 0 or Auth::user()->role == 1)
-								<li><a href="{{ route('register') }}">セッション情報</a></li>
 								<li><a href="{{ route('codemanage') }}">コード管理</a></li>
 								<li><a href="{{ route('parameter') }}">市町村パラメタ</a></li>
 								@endif
