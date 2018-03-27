cd1450e9eebb046a470562c9c7ba1535eb2d98c4
sato taro <dsjvvknu@gmail.com>
2018-03-27 16:54:21 +0900

    a

--- app/Event.php
+++ app/Event.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+
+
+
+
+	protected $fillable = [
+
+	'citycode','date','time','bunrui', 'gid1', 'gid2', 'gid3', 'meisho',
+
+	];
+
+
+	protected $primaryKey = ['gid1', 'gid2','gid3'];
+	public $incrementing = false;
+	public $timestamps = false;
+	protected $table = 'event';
+
+}
