5f650efe4e5110404c35fcbb8aaaa03146470808
sato taro <dsjvvknu@gmail.com>
2018-03-27 14:05:34 +0900

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-	<div align="center">
+	<div>
 	<a href="#" class="homebutton" onclick="location.href='/botlog'">
 	  <div>
 	    <i class="fa fa-github-alt"></i>
 	    <span>ログ参照</span>
 	  </div>
-	  <p></p>
+	  <p>CLICK !</p>
 	</a>
 	</div>
 @endsection
