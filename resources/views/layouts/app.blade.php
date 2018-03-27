4700cd115ade5063ef1ee261a0590fb87aae825d
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2018-03-27 15:32:56 +0900

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 <!-- Chart.js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
+
 
 </head>
 <body>
