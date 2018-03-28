ad8e976558e5dbe2986ff49f0731ea3d9aaddcf5
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-03-28 09:41:46 +0900

    s

--- public/js/graph.js
+++ public/js/graph.js
@@ -47,6 +47,6 @@ function init() {
 	});
 }
 function open() {
-	//var date_from = document.getElementById('date_from').value;
-	alert("aaaaaaaa");
+	$("#date_from").datepicker("getDate");
+	console.log(#date_from);
 }
