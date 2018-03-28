900f7fc765f9a25580db9dd815ef8a075fdf0040
sato taro <dsjvvknu@gmail.com>
2018-03-28 09:51:33 +0900

    a

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -89,6 +89,104 @@
 							<input id="dia_g2meisho" class="form-control" maxlength="50" placeholder="小分類名称">
 						</div>
 					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="time_hour">時間</label>
+						<div class="col-sm-2">
+							<select class="form-control" id="time_hour" >
+							<option value="1">1時</option>
+							<option value="2">2時</option>
+							<option value="3">3時</option>
+							<option value="4">4時</option>
+							<option value="5">5時</option>
+							<option value="6">6時</option>
+							<option value="7">7時</option>
+							<option value="8">8時</option>
+							<option value="9">9時</option>
+							<option value="10">10時</option>
+							<option value="11">11時</option>
+							<option value="12">12時</option>
+							<option value="13">13時</option>
+							<option value="14">14時</option>
+							<option value="15">15時</option>
+							<option value="16">16時</option>
+							<option value="17">17時</option>
+							<option value="18">18時</option>
+							<option value="19">19時</option>
+							<option value="20">20時</option>
+							<option value="21">21時</option>
+							<option value="22">22時</option>
+							<option value="23">23時</option>
+							<option value="24">24時</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="time_minute"></label>
+						<div class="col-sm-2">
+							<select class="form-control" id="time_minute" >
+							<option value="00">00</option>
+							<option value="1">1分</option>
+							<option value="2">2分</option>
+							<option value="3">3分</option>
+							<option value="4">4分</option>
+							<option value="5">5分</option>
+							<option value="6">6分</option>
+							<option value="7">7分</option>
+							<option value="8">8分</option>
+							<option value="9">9分</option>
+							<option value="10">10分</option>
+							<option value="11">11分</option>
+							<option value="12">12分</option>
+							<option value="13">13分</option>
+							<option value="14">14分</option>
+							<option value="15">15分</option>
+							<option value="16">16分</option>
+							<option value="17">17分</option>
+							<option value="18">18分</option>
+							<option value="19">19分</option>
+							<option value="20">20分</option>
+							<option value="21">21分</option>
+							<option value="22">22分</option>
+							<option value="23">23分</option>
+							<option value="24">24分</option>
+							<option value="25">25分</option>
+							<option value="26">26分</option>
+							<option value="27">27分</option>
+							<option value="28">28分</option>
+							<option value="29">29分</option>
+							<option value="30">30分</option>
+							<option value="31">31分</option>
+							<option value="32">32分</option>
+							<option value="33">33分</option>
+							<option value="34">34分</option>
+							<option value="35">35分</option>
+							<option value="36">36分</option>
+							<option value="37">37分</option>
+							<option value="38">38分</option>
+							<option value="39">39分</option>
+							<option value="40">40分</option>
+							<option value="41">41分</option>
+							<option value="42">42分</option>
+							<option value="43">43分</option>
+							<option value="44">44分</option>
+							<option value="45">45分</option>
+							<option value="46">46分</option>
+							<option value="47">47分</option>
+							<option value="48">48分</option>
+							<option value="49">49分</option>
+							<option value="50">50分</option>
+							<option value="51">51分</option>
+							<option value="52">52分</option>
+							<option value="53">53分</option>
+							<option value="54">54分</option>
+							<option value="55">55分</option>
+							<option value="56">56分</option>
+							<option value="57">57分</option>
+							<option value="58">58分</option>
+							<option value="59">59分</option>
+							</select>
+						</div>
+					</div>
 				</form>
 			</div>
 			<div class="modal-footer">
